c906e5b7c206d4c7eee63997854b56d77ac866d9
Parker Morse <flashesofpanic@gmail.com>
2012-11-05 10:23:20 -0500

    Importing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,29 @@
 LightweightStandalone::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  # HACK: Seems like these should be nested resources of the offering, but that's not really practical
+  # with the engine's URL scheme. Either way, we need to be able to optionally specify an offering ID.
+  resources :activities, :controller => 'lightweight_activities', :constraints => { :id => /\d+/ } do
+    resources :pages, :controller => 'interactive_pages', :constraints => { :id => /\d+/ } do
+      post 'add_embeddable', :on => :member
+    end
+  end
+  
+  # These don't need index or show pages - though there might be something to be said for an
+  # index .xml file as a feed for select menus - but they need create-update-delete.
+  resources :mw_interactives, :controller => 'mw_interactives', :constraints => { :id => /\d+/ }, :except => :show
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  # This is so we can build the InteractiveItem at the same time as the Interactive
+  resources :pages, :controller => 'interactive_pages', :constraints => { :id => /\d+/ } do
+    resources :mw_interactives, :controller => 'mw_interactives', :constraints => { :id => /\d+/ }, :except => :show
+  end
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  # the in-place editor needed interactive_page_path
+  resources :pages, :as => 'interactive_pages', :controller => 'interactive_pages', :constraints => { :id => /\d+/ }, :except => [:new, :create]
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # 'Show' routes including offering_ids
+  get "/activities/:id(/:offering_id)" => 'lightweight_activities#show', :as => 'lightweight_activity_show', :constraints => {:id => /\d+/, :offering_id => /\d+/}
+  get "/pages/:id(/:offering_id)" => 'interactive_pages#show', :as => 'interactive_page_show', :constraints => { :id => /\d+/, :offering_id => /\d+/ }
+  get "/activities/:activity_id/pages/:id(/:offering_id)" => 'interactive_pages#show', :as => 'activity_page_offering_show', :constraints => { :id => /\d+/, :offering_id => /\d+/, :activity_id => /\d+/ }
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # This route didn't work as a nested resource
+  post "/pages/:id/remove_embeddable/:embeddable_id" => 'interactive_pages#remove_embeddable', :as => 'page_remove_embeddable', :constraints => { :id => /\d+/, :embeddable_id => /\d+/ }
 end
