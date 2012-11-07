2a234e8c88ea0af8b9494780e030797e7f50704e
Parker Morse <flashesofpanic@gmail.com>
2012-11-07 10:42:28 -0500

    Making statuses a constant for future use

--- app/models/lightweight_activity.rb
+++ app/models/lightweight_activity.rb
@@ -1,4 +1,6 @@
 class LightweightActivity < ActiveRecord::Base
+  PUB_STATUSES = %w(draft private public archive)
+
   attr_accessible :name, :publication_status, :user_id, :pages, :related, :description
 
   belongs_to :user # Author
@@ -8,7 +10,7 @@ class LightweightActivity < ActiveRecord::Base
   default_value_for :publication_status, 'draft'
   # has_many :offerings, :dependent => :destroy, :as => :runnable, :class_name => "Portal::Offering"
 
-  validates :publication_status, :inclusion => { :in => %w(draft private public archive) }
+  validates :publication_status, :inclusion => { :in => PUB_STATUSES }
 
   def run_format
     :run_html
