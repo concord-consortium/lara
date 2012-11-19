93ef33edc809b43d7df8cee9b2b9b98693430993
Parker Morse <flashesofpanic@gmail.com>
2012-11-19 15:04:14 -0500

    Deleting empty specs for join models

--- spec/models/interactive_item_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe InteractiveItem do
-  pending "is just a join model"
-end
--- spec/models/page_item_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe PageItem do
-  pending "is just a join model"
-end
