7b1ba2cec13147aa45c9027e7a8c39ddfa6745d4
Parker Morse <flashesofpanic@gmail.com>
2012-11-20 13:22:27 -0500

    Edit a new MultipleChoice Embeddable immediately.
    This adds a little snippet of Javascript if the embeddable prompt is empty. That snippet clicks the "Edit" button automatically on page load, bringing up the edit modal for the embeddable.
    [#38432233]

--- app/models/embeddable/multiple_choice.rb
+++ app/models/embeddable/multiple_choice.rb
@@ -6,7 +6,6 @@ module Embeddable
     has_many :choices, :class_name => 'Embeddable::MultipleChoiceChoice', :foreign_key => 'multiple_choice_id'
 
     default_value_for :name, "Multiple Choice Question element"
-    default_value_for :prompt, "Why do you think ..."
 
     def create_default_choices
       self.add_choice('a')
