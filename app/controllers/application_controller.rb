4ad55d790c727676153011f9d39ae44ba184ef97
Parker Morse <pmorse@concord.org>
2013-03-04 15:31:00 -0500

    Redirect to login, not home page, on auth denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   # What to do if authorization fails
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to new_user_session_url, :alert => exception.message
   end
 
   # For modal edit windows. Source: https://gist.github.com/1456815
