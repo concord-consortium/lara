80b70e63ece3bdd5bf143232160c6f0056dc3b66
Parker Morse <pmorse@concord.org>
2013-03-11 16:06:19 -0400

    More test trimming using Factories

--- spec/models/mw_interactive_spec.rb
+++ spec/models/mw_interactive_spec.rb
@@ -1,41 +1,32 @@
 require 'spec_helper'
 
 describe MwInteractive do
-  before :each do
-    @valid = {
-      :name => "mw interactive",
-      :url  => "http://www.concord.org",
-      :width => 60,
-      :native_width => 400,
-      :native_height => 200
-    }
-    @interactive = MwInteractive.create!(@valid)
-  end
+  let (:interactive) { FactoryGirl.create(:mw_interactive) }
+  let (:page) { FactoryGirl.create(:page) }
 
   it 'has valid attributes' do
-    @interactive.name.should == "mw interactive"
-    @interactive.url.should  == "http://www.concord.org"
-    @interactive.width.should == 60
+    interactive.valid?
   end
 
   it 'can to associate an interactive page' do
-    @page = InteractivePage.create!(:name => "page", :text => "some text")
-    @page.add_interactive(@interactive)
+    
+    page.add_interactive(interactive)
 
-    @interactive.reload
-    @page.reload
+    interactive.reload
+    page.reload
 
-    @interactive.interactive_page.should == @page
+    interactive.interactive_page.should == page
   end
 
   it 'returns an aspect ratio based on native height and native width' do
-    @interactive.aspect_ratio.should === 2
-    @interactive.native_width = 500
-    @interactive.save
-    @interactive.aspect_ratio.should === 2.5
+    interactive.aspect_ratio.should === 1.3241379310344827
+    interactive.native_width = 500
+    interactive.native_height = 250
+    interactive.save
+    interactive.aspect_ratio.should === 2
   end
 
   it 'returns a calculated height from a supplied width, based on aspect ratio' do
-    @interactive.height(800).should === 400
+    interactive.height(800).should === 604.1666666666666
   end
 end
