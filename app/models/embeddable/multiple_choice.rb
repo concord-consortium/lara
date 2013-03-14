d148344f9caa6957747898b3af432d339af3e08e
Parker Morse <pmorse@concord.org>
2013-03-14 11:26:44 -0400

    Question embeddables have a storage_key

--- app/models/embeddable/multiple_choice.rb
+++ app/models/embeddable/multiple_choice.rb
@@ -3,11 +3,28 @@ module Embeddable
     self.table_name_prefix = 'embeddable_'
 
     has_many :choices, :class_name => 'Embeddable::MultipleChoiceChoice', :foreign_key => 'multiple_choice_id'
+    has_many :page_items, :as => :embeddable
+    has_many :interactive_pages, :through => :page_items
 
     attr_accessible :name, :prompt, :choices_attributes
     accepts_nested_attributes_for :choices, :allow_destroy => true
 
     default_value_for :name, "Multiple Choice Question element"
+
+    # A unique key to use for local storage
+    def storage_key
+      sk = "#{id}"
+      if name.present?
+        sk = "#{sk}_#{name.downcase.gsub(/ /, '_')}"
+      end
+      if interactive_pages.length > 0
+        if interactive_pages.first.lightweight_activity
+          sk = "#{interactive_pages.first.lightweight_activity.id}_#{interactive_pages.first.id}_#{sk}"
+        else
+          sk = "#{interactive_pages.first.id}_#{sk}"
+        end
+      end
+    end
 
     def create_default_choices
       self.add_choice('a')
