d118433d4d405184230b817e4bf3a753cde6c483
Noah Paessel <knowuh@gmail.com>
2013-04-18 19:33:07 -0400

    MultipleChoiceAnswerController: Rough draft / WIP / Placeholder

--- app/controllers/embeddable/multiple_choice_answers_controller.rb
+++ app/controllers/embeddable/multiple_choice_answers_controller.rb
@@ -1,2 +1,12 @@
 class Embeddable::MultipleChoiceAnswersController < ApplicationController
+
+  # TODO: ensure the user can change this....
+  def update
+    answer = Embeddable::MultipleChoiceAnswer.find(params[:id])
+    # TODO: something like this:
+    # params = params[:embeddable_multiple_choice_answer]
+    # answer.answer_texts = [params['answer_texts']]
+    # answer.save
+  end
+
 end
