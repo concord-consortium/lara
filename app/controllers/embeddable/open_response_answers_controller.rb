3f69c18797f32ca480dca1afd36eba3aa2a0250a
Parker Morse <pmorse@concord.org>
2013-04-22 11:50:38 -0400

    Fixing the Embeddable::OpenResponseAnswer form

--- app/controllers/embeddable/open_response_answers_controller.rb
+++ app/controllers/embeddable/open_response_answers_controller.rb
@@ -2,7 +2,6 @@ class Embeddable::OpenResponseAnswersController < ApplicationController
   # TODO: ensure the user can change this....
   def update
     answer = Embeddable::OpenResponseAnswer.find(params[:id])
-    # FIXME: This succeeds, but doesn't update because the params aren't properly structured
     if answer.update_attributes(params[:embeddable_open_response_answer])
       respond_to do |format|
         format.json { render :json => answer }
