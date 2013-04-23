794ada95e86180a37be499a96ec7dfac0f736200
Parker Morse <pmorse@concord.org>
2013-04-23 10:53:12 -0400

    Better handling of run keys for anonymous users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,10 +20,13 @@ class ApplicationController < ActionController::Base
   end
 
   def set_session_key
+    session[:response_key] ||= {}
     if params[:response_key]
       @run = Run.find_or_create_by_key_and_activity_id(params[:response_key], @activity.id)
       @session_key = @run.key
-
+    elsif session[:response_key][@activity.id]
+      @run = Run.find_or_create_by_key_and_activity_id(session[:response_key][@activity.id], @activity.id)
+      @session_key = @run.key
     elsif current_user
       @run = Run.find_or_create_by_activity_id_and_user_id(@activity.id, current_user.id)
       @session_key = @run.key
@@ -31,5 +34,7 @@ class ApplicationController < ActionController::Base
       @run = Run.create(:activity => @activity)
       @session_key = @run.key
     end
+    session[:response_key][@activity.id] = @session_key
+    # TODO: clear this hash on logout for logged-in users - requires finding callback in Devise
   end
 end
