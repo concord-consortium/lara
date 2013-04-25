e75ab0a46f075fc14089511bcb3c5c678cf073e6
Parker Morse <pmorse@concord.org>
2013-04-25 12:11:40 -0400

    Refactor OpenResponseAnswerSpec to use let

--- spec/models/embeddable/open_response_answer_spec.rb
+++ spec/models/embeddable/open_response_answer_spec.rb
@@ -1,28 +1,24 @@
 require 'spec_helper'
 
 describe Embeddable::OpenResponseAnswer do
-  before(:each) do
-    @answer = Embeddable::OpenResponseAnswer.create({
-      :answer_text          => "the answer"
-    })
-  end
+  let(:answer) { Embeddable::OpenResponseAnswer.create({ :answer_text => "the answer" }) }
 
   describe "model associations" do
 
     it "should belong to an open response" do
       question = Embeddable::OpenResponse.create()
-      @answer.question = question
-      @answer.save
-      @answer.reload.question.should == question
-      question.reload.answers.should include @answer
+      answer.question = question
+      answer.save
+      answer.reload.question.should == question
+      question.reload.answers.should include answer
     end
 
     it "should belong to a run" do
       run = Run.create(:activity => FactoryGirl.create(:activity))
-      @answer.run = run
-      @answer.save
-      @answer.reload.run.should == run
-      run.reload.open_response_answers.should include @answer
+      answer.run = run
+      answer.save
+      answer.reload.run.should == run
+      run.reload.open_response_answers.should include answer
     end
   end
 
@@ -31,8 +27,8 @@ describe Embeddable::OpenResponseAnswer do
       it "should delegate to question" do
         question = mock_model(Embeddable::OpenResponse)
         question.should_receive(:prompt).and_return(:some_prompt)
-        @answer.question = question
-        @answer.prompt.should == :some_prompt
+        answer.question = question
+        answer.prompt.should == :some_prompt
       end
     end
   end
