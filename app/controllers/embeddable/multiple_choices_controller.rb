e44b62d19cf33c406f73073e7324ff946fffba2c
Parker Morse <pmorse@concord.org>
2013-06-03 15:09:05 -0400

    Re-enables "Check Answer" for multi-answer multi-choice
    [#50756521] 
    
    https://www.pivotaltracker.com/story/show/50756521

--- app/controllers/embeddable/multiple_choices_controller.rb
+++ app/controllers/embeddable/multiple_choices_controller.rb
@@ -39,15 +39,18 @@ class Embeddable::MultipleChoicesController < ApplicationController
   end
 
   def check
-    @choice = Embeddable::MultipleChoiceChoice.find(params[:id])
+    @multiple_choice = Embeddable::MultipleChoiceAnswer.find(params[:id]).question
+    parse_choices
+    build_response
+
     if request.xhr?
       respond_to do |format|
-        format.js { render :json => @choice.to_json }
+        format.js { render :json => @response.to_json }
       end
     else
       respond_to do |format|
         format.html { redirect_to interactive_page_path(@choice.page) unless @choice.page.nil? }
-        format.json { render :json => @choice.to_json }
+        format.json { render :json => @response.to_json }
       end
     end
   end
@@ -92,6 +95,45 @@ class Embeddable::MultipleChoicesController < ApplicationController
         format.xml  { head :ok }
         format.json
       end
+    end
+  end
+
+  private
+  def parse_choices
+    begin
+      choice_ids = params[:choices].split(',').map{ |i| i.to_i }
+    rescue NoMethodError
+      choice_ids = nil
+    end
+
+    if choice_ids
+      @choices = @multiple_choice.choices.find(choice_ids)
+    else
+      @choices = []
+    end
+  end
+
+  def build_response
+    if @multiple_choice.multi_answer
+      selected_incorrect = @choices.select { |c| !c.is_correct }
+      selected_correct = @choices - selected_incorrect
+      actual_correct = @multiple_choice.choices.select { |c| c.is_correct }
+      if @choices.length == 0
+        # No answer
+        @response = { prompt: 'Please select an answer before checking.'}
+      elsif selected_incorrect.length > 0
+        # Incorrect answer(s)
+        @response = { prompt: selected_incorrect.map { |w| w.prompt.blank? ? "'#{w.choice}' is incorrect" : w.prompt }.join("; ") }
+      elsif selected_correct.length != actual_correct.length and selected_incorrect.length == 0
+        # Right answers, but not all
+        @response = { prompt: "You're on the right track, but you didn't select all the right answers yet."}
+      else selected_correct.length == actual_correct.length
+        # All correct
+        @response = { choice: true }
+      end
+    else
+      # One answer: sending the choice to get rendered as JSON by the action
+      @response = @choice = @choices.first
     end
   end
 end
--- spec/controllers/embeddable/multiple_choices_controller_spec.rb
+++ spec/controllers/embeddable/multiple_choices_controller_spec.rb
@@ -6,7 +6,7 @@ describe Embeddable::MultipleChoicesController do
   describe '#check' do
     let (:multiple) { FactoryGirl.create(:mc_embeddable, :custom => true ) }
     let (:correct) { FactoryGirl.create(:mcc_embeddable, :choice => 'This is correct', :is_correct => true, :multiple_choice => multiple) }
-    let (:incorrect) { FactoryGirl.create(:mcc_embeddable, :choice => 'This is incorrect', :is_correct => false, :multiple_choice => multiple) }
+    let (:incorrect) { FactoryGirl.create(:mcc_embeddable, :choice => 'This is incorrect', :is_correct => false, :prompt => '', :multiple_choice => multiple) }
     let (:with_prompt) { FactoryGirl.create(:mcc_embeddable, :choice => 'This is incorrect', :is_correct => false, :prompt => 'Consider what would happen if this was true.', :multiple_choice => multiple) }
     let (:answer) { FactoryGirl.create(:multiple_choice_answer, :question => multiple) }
 
@@ -29,7 +29,7 @@ describe Embeddable::MultipleChoicesController do
       end
     end
 
-    context 'there is only one answer' do
+    context 'when there is only one answer' do
       it 'returns true for a correct answer' do
         correct
         get :check, :id => answer.id, :format => 'json', :choices => correct.id.to_s
@@ -49,7 +49,7 @@ describe Embeddable::MultipleChoicesController do
       end
     end
 
-    context 'there are multiple answers' do
+    context 'when there are multiple answers' do
       let (:also_correct) { FactoryGirl.create(:mcc_embeddable, :choice => 'This is correct, too', :is_correct => true, :multiple_choice => multiple) }
 
       before(:each) do
@@ -65,12 +65,12 @@ describe Embeddable::MultipleChoicesController do
 
       it 'returns concatenated incorrect text if any submitted answers are incorrect' do
         get :check, :id => answer.id, :format => 'json', :choices => incorrect.id.to_s
-        response.body.should match /"#{incorrect.choice}" is incorrect/
+        response.body.should match /'#{incorrect.choice}' is incorrect/
       end
 
       it 'returns concatenated custom responses if incorrect answers have custom responses configured' do
-        get :check, :id => answer.id, :format => 'json', :choices => "#{incorrect.id.to_s},#{with_prompt.id.to_s}"
-        response.body.should match /"#{incorrect.choice}" is incorrect/
+        get :check, :id => answer.id, :format => 'json', :choices => "#{incorrect.id},#{with_prompt.id}"
+        response.body.should match /'#{incorrect.choice}' is incorrect/
         response.body.should match /Consider what would happen if this was true./
       end
 
@@ -82,7 +82,7 @@ describe Embeddable::MultipleChoicesController do
 
       it 'returns true if submitted answers are all correct and complete' do
         also_correct
-        get :check, :id => answer.id, :format => 'json', :choices => correct.id.to_s
+        get :check, :id => answer.id, :format => 'json', :choices => "#{correct.id},#{also_correct.id}"
         response.body.should match /"choice":[\w]*true/
       end
     end
