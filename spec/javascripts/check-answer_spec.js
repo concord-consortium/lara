f3636a106c7daa1d7b3d6ae1fd51ecd3f193072e
Parker Morse <pmorse@concord.org>
2013-06-05 11:52:53 -0400

    First round of working Jasmine specs

--- spec/javascripts/check-answer_spec.js
+++ spec/javascripts/check-answer_spec.js
@@ -1,10 +1,56 @@
 /*jslint browser: true, sloppy: true, todo: true, devel: true */
-/*global $, it, describe, xit, xdescribe, expect, loadFixtures, beforeEach */
+/*global $, it, describe, xit, xdescribe, expect, beforeEach, spyOn, CustomFeedback */
 
 //= require check-answer
 
-describe('Foo', function () {
-    it("does something", function () {
-        expect(1 + 1).toBe(2);
+describe('CustomFeedback', function () {
+    var checkbox = $('<input id="embeddable_multiple_choice_custom" name="embeddable_multiple_choice[custom]" type="checkbox" value="1">'),
+        input = $('<li class="custom-hidden" style="display: none;"></li>'),
+        custom;
+
+    beforeEach(function () {
+        custom = new CustomFeedback(input);
+    });
+
+    it('has a jQuery object attribute "el"', function () {
+        expect(custom.el).toBeDefined();
+        expect(custom.el).toBe('li.custom-hidden');
+    });
+
+    describe('show()', function () {
+        it("calls hide when checkbox is unchecked", function () {
+            spyOn(custom.el, 'hide');
+            checkbox.attr('checked', false);
+            custom.show(checkbox);
+            expect(custom.el.hide).toHaveBeenCalled();
+        });
+
+        it("calls show when checkbox is checked", function () {
+            spyOn(custom.el, 'show');
+            checkbox.attr('checked', true);
+            custom.show(checkbox);
+            expect(custom.el.show).toHaveBeenCalled();
+        });
+    });
+});
+
+describe('AnswerChecker', function () {
+
+    it('has a question id', function () {
+        expect(true).toBe(true);
+    });
+
+    it('has an answer', function () {
+        expect(true).toBe(true);
+    });
+
+    describe('check()', function () {
+        it('calls modalDialog when there are no answers', function () {
+            expect(true).toBe(true);
+        });
+
+        it('sends answers to the server', function () {
+            expect(true).toBe(true);
+        });
     });
 });
