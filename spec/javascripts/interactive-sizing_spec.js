f2c395243d6ab7c4b1aa013898781dd0c7837544
Parker Morse <pmorse@concord.org>
2013-06-14 15:22:09 -0400

    Empty spec test for sizing JS file

---
+++ spec/javascripts/interactive-sizing_spec.js
@@ -0,0 +1,22 @@
+/*jslint browser: true, sloppy: true, todo: true, devel: true */
+/*global $, it, describe, xit, xdescribe, expect, beforeEach, spyOn, loadFixtures, InteractiveObject */
+
+//= require interactive-sizing
+
+describe('InteractiveObject', function () {
+    var interactive;
+
+    // TODO: Fixtures to manipulate
+    beforeEach(function () {
+        interactive = new InteractiveObject();
+    });
+
+    // Attributes
+    it('has defined attributes', function () {
+// TODO: toBeDefined() for attributes        
+    });
+
+    describe('fixSize()', function () {
+        // TODO: call attributes on targets
+    });
+});
