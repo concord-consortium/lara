f1256e69ddc1739798ed79c29993aead5ab29fa2
Parker Morse <pmorse@concord.org>
2013-06-24 09:30:38 -0400

    Tuning exception notification configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,5 +69,5 @@ end
 
 LightweightStandalone::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[LARA Exception] ",
-  :sender_address => %{"notifier" <notifier@authoring.concord.org>},
+  :sender_address => %{"notifier" <pmorse+laranotifier@concord.org>},
   :exception_recipients => %w{pmorse@concord.org}
