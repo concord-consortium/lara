8c8e26fde08303633712edf3cb2b5539f7dc8231
Parker Morse <pmorse@concord.org>
2013-07-09 14:04:13 -0400

    Fixing up Project admin

--- spec/views/projects/index.html.haml_spec.rb
+++ spec/views/projects/index.html.haml_spec.rb
@@ -21,9 +21,6 @@ describe "projects/index" do
   it "renders a list of projects" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "Logo".to_s, :count => 2
-    assert_select "tr>td", :text => "Url".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "li.item", :count => 2
   end
 end
