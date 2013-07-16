c1e16104ce758c5149e548a11e731ed651da95f6
Parker Morse <pmorse@concord.org>
2013-07-16 09:51:28 -0400

    Nil-checking: don't look for OG logo if no logo

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,28 +5,32 @@ class Project < ActiveRecord::Base
 
   # Derive an OpenGraph-custom logo path from the provided path, or return the provided path if the OG logo doesn't exist
   def og_logo
-    logo_parts = logo.split('.')
-    if logo_parts.length > 1
-      # Insert '-og' before the extension but after the filename stem
-      stem = "#{logo_parts.first(logo_parts.length - 1).join('.')}-og"
-    else
-      # No extension: just append '-og' on the end
-      stem = "#{logo}-og"
-    end
-    # Check for stem as .jpg, .jpeg, .png and .gif, then give up
-    ogl = nil
-    %w(jpg jpeg png gif).each do |ext|
-      proposed_filename = "#{stem}.#{ext}"
-      if image_file_exists?(proposed_filename)
-        ogl = proposed_filename
-        break
+    if logo
+      logo_parts = logo.split('.')
+      if logo_parts.length > 1
+        # Insert '-og' before the extension but after the filename stem
+        stem = "#{logo_parts.first(logo_parts.length - 1).join('.')}-og"
+      else
+        # No extension: just append '-og' on the end
+        stem = "#{logo}-og"
       end
+      # Check for stem as .jpg, .jpeg, .png and .gif, then give up
+      ogl = nil
+      %w(jpg jpeg png gif).each do |ext|
+        proposed_filename = "#{stem}.#{ext}"
+        if image_file_exists?(proposed_filename)
+          ogl = proposed_filename
+          break
+        end
+      end
+      unless ogl
+        # Can't find the og_logo - just go with the project logo
+        ogl = logo
+      end
+      return ogl
+    else
+      return nil
     end
-    unless ogl
-      # Can't find the og_logo - just go with the project logo
-      ogl = logo
-    end
-    return ogl
   end
 
   # Check if the derived OpenGraph logo file exists
