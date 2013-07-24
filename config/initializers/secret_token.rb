5d38c6d58b79b8f8f5f1cf55ddc75cc26efd94cc
Parker Morse <pmorse@concord.org>
2013-07-24 13:50:12 -0400

    rm old comment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# TODO: good way to keep this out of github but deployable?
 LightweightStandalone::Application.config.secret_token = ENV['SECRET_TOKEN']
