11bf72bea909b1a9b9e1a01e9be34e3699f8823c
Parker Morse <pmorse@concord.org>
2013-07-29 09:39:00 -0400

    Assets for HAS module themes, with backgrounds
    This splits HAS into five themes, which share a common CSS file (has-styles.scss) but also have custom files which pull in different background images (and, eventually, different header graphics). This is most of [#53912853]
    
    https://www.pivotaltracker.com/story/show/53912853

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ LightweightStandalone::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( runtime.css theme-has-ngs.css theme-mw.css runtime.js respond.js)
+  config.assets.precompile += %w( runtime.css theme-has-ngs.css theme-mw.css theme-has-ngs-atmosphere.css theme-has-ngs-climage.css theme-has-ngs-hydrofracking.css theme-has-ngs-landmanagement.css theme-has-water.css runtime.js respond.js)
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'authoring.concord.org' }
