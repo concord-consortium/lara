8289144183c0b59dde4b44a8510023cf4d84fbcd
Parker Morse <pmorse@concord.org>
2013-08-02 16:07:31 -0400

    Updates behavior of sequence navigation menu
    Another step in [#51593657]. This makes all the activity links in the menu change the "active" activity, i.e. the one with the blue highlighting and the visible list of pages. Final step should be to get this behavior with the carets/arrows and the previous behavior with the text links.
    
    https://www.pivotaltracker.com/story/show/51593657

--- app/assets/javascripts/navigation-menu-drop.js
+++ app/assets/javascripts/navigation-menu-drop.js
@@ -1,23 +1,40 @@
 /*jslint browser: true, sloppy: true, todo: true, devel: true, white: true */
 /*global $ */
 
+var navMenu;
+
 var MenuControl = function (object) {
     this.trigger = object.find('a#menu-trigger');
     this.menu = object.find('div.nav-menu-top');
-    var menu = this.menu;
+    var menu = this.menu,
+        self = this;
+    // Add click handler to the trigger object, to open and close the menu
     this.trigger.click(function () {
         menu.toggle();
     });
+    // Which menu element is active?
+    this.active = this.menu.find('.active');
+    // Add click handlers to links to change active activity
+    this.menu.find('li.activity>a').each( function () {
+        $(this).on('click', function () {
+            self.changeActive($(this).parent());
+        });
+    });
 };
 
-MenuControl.prototype.open = function () {
-    this.menu.toggle();
-};
-
-MenuControl.prototype.close = function () {
-    this.menu.toggle();
+MenuControl.prototype.changeActive = function (element) {
+    // Show the new menu
+    element.addClass('active');
+    // Fix its caret
+    element.find('i.icon-angle-up').addClass('icon-angle-down').removeClass('icon-angle-up');
+    // Fix the old activity's caret
+    this.active.find('i.icon-angle-down').addClass('icon-angle-up').removeClass('icon-angle-down');
+    // Close the old activity
+    this.active.removeClass('active');
+    // Re-set current-active
+    this.active = element;
 };
 
 $(document).ready(function () {
-    var navMenu = new MenuControl($('div.nav-menu'));
+    navMenu = new MenuControl($('div.nav-menu'));
 });
--- spec/javascripts/navigation-menu-drop_spec.js
+++ spec/javascripts/navigation-menu-drop_spec.js
@@ -6,40 +6,74 @@
 describe('MenuControl', function () {
     var menu;
 
-    beforeEach(function () {
-        loadFixtures('navigation-menu.html');
-        menu = new MenuControl($('div.nav-menu'));
+    describe('basic menu function', function () {
+        beforeEach(function () {
+            loadFixtures('navigation-menu.html');
+            menu = new MenuControl($('div.nav-menu'));
+        });
+
+        it('has a jQuery object attribute "trigger"', function () {
+            expect(menu.trigger).toBeDefined();
+            expect(menu.trigger).toBe('a#menu-trigger');
+        });
+
+        it('has a jQuery object attribute "menu"', function () {
+            expect(menu.menu).toBeDefined();
+            expect(menu.menu).toBe('div.nav-menu-top');
+        });
+
+        it('has a jQuery object attribute "active"', function () {
+            expect(menu.active).toBeDefined();
+        });
+
+        it('calls `toggle()` on the menu when trigger is clicked', function () {
+            spyOn(menu.menu, 'toggle');
+            menu.trigger.click();
+            expect(menu.menu.toggle).toHaveBeenCalled();
+        });
+
+        it('is not showing the menu at page load', function () {
+            expect(menu.menu.css('display')).toBe('none');
+        });
+
+        it('is showing the menu after one click', function () {
+            menu.trigger.click();
+            expect(menu.menu.css('display')).toBe('block');
+        });
+
+        it('hides the menu on the second click', function () {
+            menu.trigger.click();
+            menu.trigger.click();
+            expect(menu.menu.css('display')).toBe('none');
+        });
     });
 
-    it('has a jQuery object attribute "trigger"', function () {
-        expect(menu.trigger).toBeDefined();
-        expect(menu.trigger).toBe('a#menu-trigger');
+    describe('when the menu has a sequence', function () {
+        beforeEach(function () {
+            loadFixtures('navigation-menu-sequence.html');
+            menu = new MenuControl($('div.nav-menu'));
+        });
+
+        it('has an element in the "active" attribute', function () {
+            expect(menu.active).toBe('li.activity.active');
+        });
+
+        it('calls changeActive when activities are clicked', function () {
+            spyOn(menu, 'changeActive');
+            $('.activity>a').first().trigger('click');
+            expect(menu.changeActive).toHaveBeenCalled();
+        });
+
+        describe('changeActive', function () {
+            it('changes the currently active activity', function () {
+                var currActive = menu.active;
+                menu.changeActive($('.activity>a').first().parent());
+                expect(menu.active).not.toEqual(currActive);
+                expect(currActive).not.toBe('li.activity.active');
+                expect(menu.active).toBe('li.activity.active');
+                expect(menu.active.find('ol li.page')[0]).toBeVisible();
+            });
+        });
     });
 
-    it('has a jQuery object attribute "menu"', function () {
-        expect(menu.menu).toBeDefined();
-        expect(menu.menu).toBe('div.nav-menu-top');
-    });
-
-    it('calls `toggle()` on the menu when trigger is clicked', function () {
-        // spyon
-        spyOn(menu.menu, 'toggle');
-        menu.trigger.click();
-        expect(menu.menu.toggle).toHaveBeenCalled();
-    });
-
-    it('is not showing the menu at page load', function () {
-        expect(menu.menu.css('display')).toBe('none');
-    });
-
-    it('is showing the menu after one click', function () {
-        menu.trigger.click();
-        expect(menu.menu.css('display')).toBe('block');
-    });
-
-    it('hides the menu on the second click', function () {
-        menu.trigger.click();
-        menu.trigger.click();
-        expect(menu.menu.css('display')).toBe('none');
-    });
 });
