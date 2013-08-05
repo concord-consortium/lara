5863261466eae315fa3d586f3fcd083df48bd4f8
Parker Morse <pmorse@concord.org>
2013-08-05 12:34:02 -0400

    Nav menu: caret opens page list, activity name is link
    [#51593657]
    
    https://www.pivotaltracker.com/story/show/51593657

--- app/assets/javascripts/navigation-menu-drop.js
+++ app/assets/javascripts/navigation-menu-drop.js
@@ -6,31 +6,33 @@ var navMenu;
 var MenuControl = function (object) {
     this.trigger = object.find('a#menu-trigger');
     this.menu = object.find('div.nav-menu-top');
+    // Which menu element is active?
+    this.active = this.menu.find('.active');
+    // Which element is open?
+    this.openActivity = this.menu.find('.on');
     var menu = this.menu,
         self = this;
     // Add click handler to the trigger object, to open and close the menu
     this.trigger.click(function () {
         menu.toggle();
     });
-    // Which menu element is active?
-    this.active = this.menu.find('.active');
     // Add click handlers to links to change active activity
-    this.menu.find('li.activity>a').each( function () {
+    this.menu.find('li.activity a.open-close').each( function () {
         $(this).on('click', function () {
-            self.changeActive($(this).parent());
+            self.changeActive($(this).parent().parent());
         });
     });
 };
 
 MenuControl.prototype.changeActive = function (element) {
     // Show the new menu
-    element.addClass('active');
+    element.addClass('on');
     // Fix its caret
     element.find('i.icon-angle-up').addClass('icon-angle-down').removeClass('icon-angle-up');
     // Fix the old activity's caret
     this.active.find('i.icon-angle-down').addClass('icon-angle-up').removeClass('icon-angle-down');
     // Close the old activity
-    this.active.removeClass('active');
+    this.active.removeClass('on');
     // Re-set current-active
     this.active = element;
 };
