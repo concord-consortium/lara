af59f2dc3be357c5f58683bac125cd239309d6b3
Noah Paessel <knowuh@gmail.com>
2013-08-27 10:31:02 -0400

    This test is working for me, un-pending it.

--- spec/features/activity_run_from_portal_spec.rb
+++ spec/features/activity_run_from_portal_spec.rb
@@ -4,7 +4,7 @@ require 'uri'
 feature "Activity is run from the portal", :js => true do
 
   scenario "with a domain, external_id, and return url" do
-    pending "need to fix url redirection in application controller."
+
     # need an activity with at least a open response question
   	activity = FactoryGirl.create(:activity_with_page_and_or)
     user     = FactoryGirl.create(:user_with_authentication)
