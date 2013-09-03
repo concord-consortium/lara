1d66878c88929c3d318a35d5639fbf46032009d5
Noah Paessel <knowuh@gmail.com>
2013-09-03 11:14:59 -0400

    Sequence: add publication_status, and is_offical for activity parity.

--- db/migrate/20130830204358_add_publication_status_and_is_official_to_sequence.rb
+++ db/migrate/20130830204358_add_publication_status_and_is_official_to_sequence.rb
@@ -1,6 +1,18 @@
 class AddPublicationStatusAndIsOfficialToSequence < ActiveRecord::Migration
-  def change
+  class Sequence < ActiveRecord::Base
+    attr_protected :non_existint_field # make other attrs accessable.
+  end
+
+  def up
     add_column :sequences, :publication_status, :string, :default => 'draft'
     add_column :sequences, :is_official, :boolean, :default => false
+    # make existing sequences have 'public' as publication status
+    Sequence.reset_column_information
+    Sequence.all.each { |s| s.update_attributes(publication_status: 'public', is_official: true)}
   end
+  def down
+    remove_column :sequences, :publication_status
+    remove_column :sequences, :is_official
+  end
+
 end
