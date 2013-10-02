d90f763b843bba713820e2af0b5b60d60d044562
Noah Paessel <knowuh@gmail.com>
2013-10-02 14:49:33 -0400

    This looks like a big commit, but it's just adding module Embeddable.
    git diff made a mess of things, but the change is small.
    The file used to read:
    
    class Embeddable::ImageQuestionAnswer < ActiveRecord
      include Answer
      …
    end
    
    But now it goes like this:
    
    module Embeddable
      class ImageQuestionAnswer < ActiveRecord
    	include Answer
        …
      end
    end

--- app/models/embeddable/image_question_answer.rb
+++ app/models/embeddable/image_question_answer.rb
@@ -1,38 +1,40 @@
-class Embeddable::ImageQuestionAnswer < ActiveRecord::Base
-  include Answer
+module Embeddable
+  class ImageQuestionAnswer < ActiveRecord::Base
+    include Answer
 
-  attr_accessible :answer_text, :image_url, :run, :question, :annotation, :annotated_image_url
+    attr_accessible :answer_text, :image_url, :run, :question, :annotation, :annotated_image_url
 
-  belongs_to :question,
-    :class_name => 'Embeddable::ImageQuestion',
-    :foreign_key => "image_question_id"
+    belongs_to :question,
+      :class_name => 'Embeddable::ImageQuestion',
+      :foreign_key => "image_question_id"
 
-  belongs_to :run
+    belongs_to :run
 
-  scope :by_question, lambda { |q|
-    {:conditions => { :image_question_id => q.id}}
-  }
-
-  scope :by_run, lambda { |r|
-    {:conditions => { :run_id => r.id }}
-  }
-
-  delegate :prompt,  :to  => :question
-  delegate :name,    :to  => :question
-
-  after_update :send_to_portal
-
-  def has_snapshot?
-    !image_url.blank?
-  end
-
-  def portal_hash
-    {
-      "type" => "image_question",
-      "question_id" => question.id.to_s,
-      "answer" => answer_text,
-      "image_url" => annotated_image_url || image_url,
-      "annotation" => annotation
+    scope :by_question, lambda { |q|
+      {:conditions => { :image_question_id => q.id}}
     }
+
+    scope :by_run, lambda { |r|
+      {:conditions => { :run_id => r.id }}
+    }
+
+    delegate :prompt,  :to  => :question
+    delegate :name,    :to  => :question
+
+    after_update :send_to_portal
+
+    def has_snapshot?
+      !image_url.blank?
+    end
+
+    def portal_hash
+      {
+        "type" => "image_question",
+        "question_id" => question.id.to_s,
+        "answer" => answer_text,
+        "image_url" => annotated_image_url || image_url,
+        "annotation" => annotation
+      }
+    end
   end
 end
