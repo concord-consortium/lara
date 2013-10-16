eb26773b2dafc93973cede212245edeb6e348e20
Parker Morse <pmorse@concord.org>
2013-10-16 10:01:08 -0400

    Don't add interactive-pinning waypoints if not needed
    For [#53568273] and may also help with [#53579129]. This checks to see if there's actually enough height in the page for the interactive to move if it's pinned, or if it would wind up running down over footer content (or back up over header content). If there's not enough space, the waypoints aren't set. This should fix a lot of weird scrolling behavior when the interactive block is close to the height of the info/assessment block (or taller than it).
    
    https://www.pivotaltracker.com/story/show/53568273
    https://www.pivotaltracker.com/story/show/53579129

--- app/assets/javascripts/scroll-interactive-mod.js
+++ app/assets/javascripts/scroll-interactive-mod.js
@@ -6,14 +6,15 @@ var InteractiveModule = function (object) {
     this.module = object;
     this.i_width = object.width();
     this.i_height = object.height();
+    this.b_margin = parseInt(object.css('margin-bottom'), 10);
 };
 
 // Fixes the interactive mod when the window hits the questions scrolling down
 InteractiveModule.prototype.fixTop = function () {
     this.module.addClass('stuck');
-	this.module.css({
-		'width': this.i_width
-	});
+    this.module.css({
+        'width': this.i_width
+    });
 };
 
 // Un-fixes the interactive mod when the window hits the questions scrolling up
@@ -36,37 +37,54 @@ InteractiveModule.prototype.getHeight = function () {
     return this.i_height;
 };
 
+InteractiveModule.prototype.getBottomMargin = function () {
+    return this.b_margin;
+};
+
+InteractiveModule.prototype.getHeightWithMargin = function () {
+    return this.i_height + this.b_margin;
+};
+
 $(document).ready(function () {
     var scrollingInteractive = new InteractiveModule($('.pinned'));
 
-	// These blocks use jquery.waypoints
-    // Set the top waypoint and call relevant functions from it
-    $('.content-mod').waypoint( function (direction) {
-		if (direction==='down') {
-            scrollingInteractive.fixTop();
-            $('.questions-full').css({
-                'margin-top': this.i_height+52
-            });
-		}
-		if (direction==='up') {
-            scrollingInteractive.unFixTop();
-            $('.questions-full').css({
-                'margin-top': 0
-            });
-		}
-	}, {
-        offset: 120 // Is this right for all layouts?
-    });
+    // Does the interactive actually have enough room to scroll? It does if its height is
+    // less than the difference between its starting position ($('.content-mod').offset().top)
+    // and the bottom of the scroll track ($('#end-scroll-track').offset().top). (I've added 
+    // a few pixels to that Just In Case - we don't need to wiggle back and forth 10 pixels just
+    // because there's room to do so.)
+    if (scrollingInteractive.getHeightWithMargin() < ($('#end-scroll-track').offset().top - $('.content-mod').offset().top) - 10) {
+        // These blocks use jquery.waypoints
+        // Set the top waypoint and call relevant functions from it
+        $('.content-mod').waypoint( function (direction) {
+            if (direction==='down') {
+                scrollingInteractive.fixTop();
+                $('.questions-full').css({
+                    'margin-top': scrollingInteractive.getHeightWithMargin() // The 52 is a magic number for the bottom margin
+                });
+            }
+            if (direction==='up') {
+                scrollingInteractive.unFixTop();
+                $('.questions-full').css({
+                    'margin-top': 0
+                });
+            }
+        }, {
+            context: '#container',
+            offset: 120 // Is this right for all layouts?
+        });
 
-    // Set the bottom waypoint and call the relevant function there too
-	$('#end-scroll-track').waypoint(function (direction) {
-		if (direction === 'down') {
-            scrollingInteractive.unFixBottom();
-		}
-		if (direction === 'up') {
-            scrollingInteractive.fixBottom();
-		}
-	}, { 
-        offset: scrollingInteractive.getHeight() + 180
-    });
+        // Set the bottom waypoint and call the relevant function there too
+        $('#end-scroll-track').waypoint(function (direction) {
+            if (direction === 'down') {
+                scrollingInteractive.unFixBottom();
+            }
+            if (direction === 'up') {
+                scrollingInteractive.fixBottom();
+            }
+        }, {
+            context: '#container',
+            offset: scrollingInteractive.getHeight() + 180
+        });
+    }
 });
