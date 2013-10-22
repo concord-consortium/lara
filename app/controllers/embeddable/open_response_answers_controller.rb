9f1e3ad07b3b541262d088d9649b73a24edd4e1b
Parker Morse <pmorse@concord.org>
2013-10-22 11:20:14 -0400

    Tweaks to squeeze speed on OpenResponseAnswersController

--- app/controllers/embeddable/open_response_answers_controller.rb
+++ app/controllers/embeddable/open_response_answers_controller.rb
@@ -2,12 +2,10 @@ class Embeddable::OpenResponseAnswersController < ApplicationController
 
   def update
     answer = Embeddable::OpenResponseAnswer.find(params[:id])
-    if answer.update_attributes(params[:embeddable_open_response_answer])
-      respond_to do |format|
+    respond_to do |format|
+      if answer.update_attributes(params[:embeddable_open_response_answer])
         format.json { render :json => answer.to_json }
-      end
-    else
-      respond_to do |format|
+      else
         format.json { render :json => answer.errors }
       end
     end
