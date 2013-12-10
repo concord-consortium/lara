0ac84e389b88fa2ff70ee3608a59c9e9b2fcfd61
Parker Morse <pmorse@concord.org>
2013-12-10 13:35:30 -0500

    WIP comments

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def concord_portal
-    omniauth = request.env["omniauth.auth"]
+    omniauth = request.env["omniauth.auth"] # Can this object have multiple portal options?
     @user = User.find_for_concord_portal_oauth(omniauth, current_user)
     sign_in_and_redirect @user, :event => :authentication
   end
