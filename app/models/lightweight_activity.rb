c28d6e2460f84bf93d6af2ce78a0713be5e49251
Parker Morse <pmorse@concord.org>
2013-12-11 14:32:06 -0500

    Add `notes` to the duplicated activity data.
    [#56448800] https://www.pivotaltracker.com/story/show/56448800

--- app/models/lightweight_activity.rb
+++ app/models/lightweight_activity.rb
@@ -69,7 +69,8 @@ class LightweightActivity < ActiveRecord::Base
       time_to_complete: time_to_complete,
       project_id: project_id,
       theme_id: theme_id,
-      thumbnail_url: thumbnail_url
+      thumbnail_url: thumbnail_url,
+      notes: notes
     }
   end
 
