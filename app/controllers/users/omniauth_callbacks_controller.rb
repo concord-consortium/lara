9646b1256944adc8f5171641d325178ca123d4c6
Parker Morse <pmorse@concord.org>
2013-12-16 16:14:30 -0500

    Should be enough to accept users from any portal.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def concord_portal
-    omniauth = request.env["omniauth.auth"] # Can this object have multiple portal options?
+    omniauth = request.env["omniauth.auth"]
     @user = User.find_for_concord_portal_oauth(omniauth, current_user)
+    # TODO: Store portal signed in from?
     sign_in_and_redirect @user, :event => :authentication
   end
 end
