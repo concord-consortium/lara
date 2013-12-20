b4240b30d1d50ed403d3a2e22c15e6b4801a3329
Parker Morse <pmorse@concord.org>
2013-12-20 10:03:40 -0500

    A to-do list for testing the ImageQuestion JS

---
+++ spec/javascripts/image_question_spec.js
@@ -0,0 +1,33 @@
+/*jslint browser: true, sloppy: true, todo: true, devel: true */
+/*global $, it, describe, xit, xdescribe, expect, beforeEach, loadFixtures, spyOn, MenuControl */
+
+//= require image_question
+
+describe('ImageQuestion', function () {
+    // TODO: Fixture to feed to the constructor
+    
+    // TODO: Test object created by constructor with fixture input for properties
+    // TODO: Test hooks: send events and verify responses
+    // TODO: update_display()
+    // TODO: show_edit_buttons
+    // TODO: get_svg_canvas
+    // TODO: set_svg_background
+    // TODO: save_failed
+    // TODO: show_saved
+    // TODO: show_saving
+    // TODO: save
+    // TODO: set_image_source
+    // TODO: set_image
+    // TODO: set_svg_input
+    // TODO: submit_svg_form
+    // TODO: reset_image
+    // TODO: clear_text_response
+    // TODO: delete_image
+    // TODO: snapshot_updater
+    // TODO: hide
+    describe('something', function () {
+        it('Does something', function () {
+            expect(true);
+        });
+    });
+});
