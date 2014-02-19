3a41950ddd79c735a912a82f6d198c8758c1d310
Noah Paessel <knowuh@gmail.com>
2014-02-19 15:52:35 -0500

    lightweight_activity_controller_spec.rb : Remove publication related tests

--- spec/controllers/lightweight_activity_controller_spec.rb
+++ spec/controllers/lightweight_activity_controller_spec.rb
@@ -389,43 +389,6 @@ describe LightweightActivitiesController do
     end
   end
 
-  describe "#publish" do
-    let(:act_one) do
-      LightweightActivity.create!(:name => 'Activity One',
-                                  :description => 'Activity One Description',
-                                  :publication_status => 'public')
-    end
-    #let(:good_body) { act_one.serialize_for_portal('http://test.host').to_json }
-
-    let(:good_body) { "{\"type\":\"Activity\",\"name\":\"Activity One\",\"description\":\"Activity One Description\",\"url\":\"http://test.host/activities/#{act_one.id}\",\"create_url\":\"http://test.host/activities/#{act_one.id}\",\"sections\":[{\"name\":\"Activity One Section\",\"pages\":[]}]}" }
-
-    before(:each) do
-      @url = controller.portal_url
-      stub_request(:post, @url)
-    end
-
-    it "should call 'publish!' on the activity" do
-      get :publish, { :id => act_one.id }
-      act.publication_status.should == 'public'
-    end
-
-    it "should attempt to publish to the correct portal endpoint" do
-      pending "New portal configuration"
-      @url.should == "#{ENV['CONCORD_PORTAL_URL']}/external_activities/publish/v2"
-    end
-
-    it "should attempt to publish to the portal" do
-      get :publish, { :id => act_one.id }
-      WebMock.should have_requested(:post, @url).with(:body => good_body, :headers => {'Authorization'=>'Bearer', 'Content-Type'=>'application/json'})
-    end
-
-    it 'creates a new PortalPublication record' do
-      old_publication_count = act_one.portal_publications.length
-      get :publish, { :id => act_one.id }
-      act_one.reload.portal_publications.length.should == old_publication_count + 1
-    end
-  end
-
   describe '#duplicate' do
     it "should call 'duplicate' on the activity" do
       get :duplicate, { :id => act.id }
