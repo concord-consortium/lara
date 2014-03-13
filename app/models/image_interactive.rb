798a31c98206c31f8abbecac6be29eb63fc4c552
Noah Paessel <knowuh@gmail.com>
2014-03-13 14:52:26 -0400

    Allow authors to write HTML if they want to for the credits: [#64233824]
    https://www.pivotaltracker.com/story/show/64233824

--- app/models/image_interactive.rb
+++ app/models/image_interactive.rb
@@ -24,7 +24,7 @@ class ImageInteractive < ActiveRecord::Base
   end
 
   def credit_with_link
-    return self.credit if self.credit_url.blank?
+    return self.credit.html_safe if self.credit_url.blank?
     return "<a href='#{self.credit_url}' target='_blank'>#{self.credit}</a>".html_safe
   end
 end
