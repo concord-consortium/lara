46c097254e517dcb55957446bc39343bd4cc1da7
Noah Paessel <knowuh@gmail.com>
2014-04-30 11:31:10 -0400

    Completed runtime implementation for open response prediction questions
    [#69960404]
    https://www.pivotaltracker.com/story/show/69960404

--- app/assets/javascripts/prediction.coffee
+++ app/assets/javascripts/prediction.coffee
@@ -1,18 +1,33 @@
 class Prediction
 
   constructor: (@$button) ->
-    @$form = @$button.parent()
+    @$form = @$button.parent().parent()
     @$is_final = @$form.find('.hidden_is_final')
-    @$button.click (e) =>
-      @$is_final.val('1')
-      window.SaveOnChangePage.instance.force_save_item(@$form)
-      @toggle()
+    if @$is_final.val() == "t"
+      @disable_form()
+    else
+      @enable_form()
 
-  toggle: ->
+    @$button.click (e) =>
+      @record_answer()
+      @disable_form()
+
+  record_answer: ->
+    @$is_final.val('1')
+    Prediction.saver.force_save_item(@$form)
+
+  enable_form: ->
+    @$form.find(':input').prop("disabled", false);
+    @$form.find('.still_answering').show()
+    @$form.find('.is_final').hide()
+
+  disable_form: ->
     @$form.find(':input').prop("disabled", true);
-    @$form.find('.prediction_button').hide() 
-    @$form.find('.answer_is_final').show() 
+    @$form.find('.still_answering').hide()
+    @$form.find('.is_final').show()
+
 window.Prediction = Prediction
 $(document).ready ->
+  Prediction.saver = window.SaveOnChangePage.instance
   $('.prediction_button').each (i,$pred) ->
     p = new Prediction($($pred))
--- app/helpers/prediction_button_helper.rb
+++ app/helpers/prediction_button_helper.rb
@@ -6,21 +6,23 @@ module PredictionButtonHelper
     # render the button
     capture_haml do
       haml_concat form.hidden_field :is_final,  :class => 'hidden_is_final'
-      haml_tag :button,
-        :type => 'button',
-        :label => t(:PREDICTION_BUTTON),
-        :name  => "is_final",
-        :value => t(:PREDICTION_BUTTON),
-        'data-is-final' => emb.is_final,
-        :class => 'button prediction_button' do
-           haml_tag :span do
-             haml_concat t(:PREDICTION_BUTTON)
-           end
-           haml_tag :i, :class => "fa fa-unlock"
-           haml_tag :i, :class => "fa fa-lock"
+      haml_tag :div, :class => 'still_answering' do
+        haml_tag :button,
+          :type => 'button',
+          :label => t(:PREDICTION_BUTTON),
+          :name  => "is_final",
+          :value => t(:PREDICTION_BUTTON),
+          :class => 'button prediction_button' do
+             haml_tag :span do
+               haml_concat t(:PREDICTION_BUTTON)
+             end
+             haml_tag :i, :class => "fa fa-lock"
+          end
+      end
+      haml_tag :div, :class => 'is_final' do
+        haml_tag :div, :class => 'answer_is_final', 'data-is-final' => emb.is_final do
+          haml_concat t(:ANSWER_IS_FINAL)
         end
-      haml_tag :div, :class => 'answer_is_final', 'data-is-final' => emb.is_final do
-        haml_concat t(:ANSWER_IS_FINAL)
       end
     end
   end
