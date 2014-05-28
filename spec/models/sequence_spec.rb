30ece9aa39abffbc01e244b78bd2cb5584e88629
Piotr Janik <janikpiotrek@gmail.com>
2014-05-28 18:57:09 +0200

    Add specs of Sequence #duplicate and #to_hash methods
    [#55911802]

--- spec/models/sequence_spec.rb
+++ spec/models/sequence_spec.rb
@@ -4,6 +4,7 @@ describe Sequence do
   let (:sequence) { FactoryGirl.create(:sequence) }
   let (:activity1) { FactoryGirl.create(:activity, :time_to_complete => 45) }
   let (:activity2) { FactoryGirl.create(:activity, :time_to_complete => 40) }
+  let(:thumbnail_url) { "http://i.huffpost.com/gen/1469621/thumbs/o-MARK-TWAIN-facebook.jpg" }
 
   it 'has valid attributes' do
     sequence.should be_valid
@@ -54,7 +55,6 @@ describe Sequence do
   end
 
   describe '#serialize_for_portal' do
-    let(:thumbnail_url) { "http://i.huffpost.com/gen/1469621/thumbs/o-MARK-TWAIN-facebook.jpg" }
     let(:act1) { FactoryGirl.build(:activity_with_page) }
     let(:sequence_with_activities) do
       seq = FactoryGirl.build(:sequence)
@@ -92,6 +92,57 @@ describe Sequence do
       sequence_with_activities.serialize_for_portal("http://test.host").should == complex_portal_hash
     end
 
+  end
+
+  describe '#to_hash' do
+    it 'returns a hash with relevant values for sequence duplication' do
+      expected = { title: sequence.title, description: sequence.description, theme_id: sequence.theme_id, project_id: sequence.project_id,
+                   logo: sequence.logo, display_title: sequence.display_title, thumbnail_url: sequence.thumbnail_url }
+      sequence.to_hash.should == expected
+    end
+  end
+
+  describe '#duplicate' do
+    let(:sequence_with_activities) do
+      seq = FactoryGirl.build(:sequence)
+      seq.thumbnail_url = thumbnail_url
+      seq.activities << FactoryGirl.build(:activity_with_page)
+      seq.activities << FactoryGirl.build(:activity_with_page)
+      seq.activities << FactoryGirl.build(:activity_with_page)
+      seq.save!
+      seq
+    end
+
+    it 'creates a new Sequence with attributes from the original' do
+      dup = sequence.duplicate
+      dup.should be_a(Sequence)
+      dup.title.should == "Copy of #{sequence.title}"
+      dup.description.should == sequence.description
+      dup.theme_id.should == sequence.theme_id
+      dup.project_id.should == sequence.project_id
+      dup.logo.should == sequence.logo
+      dup.display_title.should == sequence.display_title
+      dup.thumbnail_url.should == sequence.thumbnail_url
+      dup.activities.length.should == sequence.activities.length
+    end
+
+    it 'performs deep copy of all activities included in a given sequence' do
+      dup = sequence_with_activities.duplicate
+      dup.activities.length.should == sequence_with_activities.activities.length
+      dup.activities.zip(sequence_with_activities.activities).each do |a, b|
+        a.should_not == b # deep copy!
+        a.name.should == b.name # we don't want "Copy of" prefix here
+        a.related.should == b.related
+        a.description.should == b.description
+        a.time_to_complete.should == b.time_to_complete
+      end
+    end
+
+    it 'keeps order of activities from the original' do
+      dup.lightweight_activities_sequences.zip(sequence_with_activities.lightweight_activities_sequences) do |a, b|
+        a.position.should == b.position
+      end
+    end
   end
 
 end
