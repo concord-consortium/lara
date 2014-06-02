4be17cf63dffb932459a74199eff24f17b5f0885
Noah Paessel <knowuh@gmail.com>
2014-06-02 09:57:59 -0400

    Use AWS::SES For Action mailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,13 +1,4 @@
-# Whitelist config options for SMTP
-CONFIG_OPTIONS = %w(default_options logger smtp_settings sendmail_settings file_settings raise_delivery_errors delivery_method perform_deliveries deliveries)
-
-if File.exists?("#{::Rails.root.to_s}/config/mailer.yml") || ::Rails.env == "test" || ::Rails.env == "cucumber"
-  require "action_mailer"
-  if ::Rails.env == "test" || ::Rails.env == "cucumber"
-    puts "Overriding ActionMailer config and setting test mode"
-    ActionMailer::Base.delivery_method = :test
-  else
-    config_file = Rails.root.join("config", "mailer.yml")
-    ActionMailer::Base.smtp_settings = YAML.load_file(config_file)[Rails.env].try(:to_options)
-  end
-end
+# Use new AWS::SES mailer method from 'aws-ses' gem
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+    access_key_id: ENV['SES_KEY'],
+    secret_access_key: ENV['SES_SECRET']
