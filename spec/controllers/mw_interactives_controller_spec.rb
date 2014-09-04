4f558ceee3788270d14c315be9211d973e43a486
Noah Paessel <knowuh@gmail.com>
2014-09-04 14:32:08 -0400

    Fix failing specs after updating rails. (3.2.17 → 3.2.19)

--- spec/controllers/mw_interactives_controller_spec.rb
+++ spec/controllers/mw_interactives_controller_spec.rb
@@ -83,7 +83,8 @@ describe MwInteractivesController do
           get :edit, :id => int.id, :page_id => page.id, :format => 'js'
 
           response.headers['Content-Type'].should match /text\/json/
-          response.body.should match /<form[^>]+action=\\"\/pages\/#{page.id}\/mw_interactives\/#{int.id}\\"[^<]+method=\\"post\\"[^<]*>/
+          value_hash = JSON.parse(response.body)
+          value_hash['html'].should match %r[<form[^>]+action=\"/pages\/#{page.id}\/mw_interactives\/#{int.id}\"[^<]+method=\"post]
         end
       end
 
--- spec/controllers/video_interactives_controller_spec.rb
+++ spec/controllers/video_interactives_controller_spec.rb
@@ -76,7 +76,8 @@ describe VideoInteractivesController do
           get :edit, :id => int.id, :page_id => page.id, :format => 'js'
 
           response.headers['Content-Type'].should match /text\/json/
-          response.body.should match /<form[^>]+action=\\"\/pages\/#{page.id}\/video_interactives\/#{int.id}\\"[^<]+method=\\"post\\"[^<]*>/
+          value_hash = JSON.parse(response.body)
+          value_hash['html'].should match %r[<form[^>]+action=\"/pages\/#{page.id}\/video_interactives\/#{int.id}\"[^<]+method=\"post]
         end
       end
 
