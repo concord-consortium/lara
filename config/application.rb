530b51ccabb74db28353099f1bce1dc49295e4f7
Noah Paessel <knowuh@gmail.com>
2014-09-04 17:17:28 -0400

    silence i18n deprecation warnings.  Some day, we may flip this to true.

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module LightweightStandalone
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.i18n.enforce_available_locales = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
