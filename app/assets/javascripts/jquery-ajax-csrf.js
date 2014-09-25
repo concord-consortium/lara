5c7a40494d81a8d7c01852563d5229c2006147bf
Piotr Janik <janikpiotrek@gmail.com>
2014-09-25 15:27:09 +0200

    Don't always include X-CSRF-Token header (was breaking Shutterbug)

--- app/assets/javascripts/jquery-ajax-csrf.js
+++ app/assets/javascripts/jquery-ajax-csrf.js
@@ -2,9 +2,18 @@
 // Note that this file should be included right after jQuery and before other scripts.
 $(document).ready(function () {
   var token = $('meta[name="csrf-token"]').attr('content');
+  // "Free" URI parser in JS.
+  var urlParser = document.createElement('a');
   $.ajaxSetup({
-    beforeSend: function (xhr) {
-      xhr.setRequestHeader('X-CSRF-Token', token);
+    beforeSend: function (xhr, settings) {
+      // Note that <a> element will set .hostname to correct value even
+      // if we provide relative path like '/api/v1/something'.
+      // Set X-CSRF-Token only while issuing request to LARA API,
+      // as this header was e.g. breaking Shutterbug service.
+      urlParser.href = settings.url;
+      if (urlParser.hostname == window.location.hostname) {
+        xhr.setRequestHeader('X-CSRF-Token', token);
+  	  }
     }
   });
 });
