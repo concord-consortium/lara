5a28d580c7350e53191ea9a86636282acfccd2a8
Noah Paessel <knowuh@gmail.com>
2014-10-08 17:36:46 -0400

    Improve performance of report page:  AnswerFinder preloads answers :  [#66528200]
    Uses local hash lookup to find answers instead of n+1 SQL queries.
    
    https://www.pivotaltracker.com/story/show/66528200

--- app/models/embeddable/answer_finder.rb
+++ app/models/embeddable/answer_finder.rb
@@ -1,20 +1,48 @@
 module Embeddable
   class AnswerFinder
-    attr_accessor :run
+    attr_accessor :run, :answer_map
+
+    def question_key(question)
+      case question
+      when Embeddable::OpenResponse
+        return "or_#{question.id}"
+      when Embeddable::ImageQuestion
+        return "iq_#{question.id}"
+      when Embeddable::MultipleChoice
+        return "mc_#{question.id}"
+      end
+      return nil
+    end
+
+    def add_answer(a)
+      self.answer_map[question_key(a.question)] = a
+    end
 
     def initialize(_run=Run.create())
+      self.answer_map = {}
       self.run = _run
+      mc_answers = self.run.multiple_choice_answers.includes(:question, :answers).each { |a| add_answer(a) }
+      or_answers = self.run.open_response_answers.includes(:question).each   { |a| add_answer(a) }
+      iq_answers = self.run.image_question_answers.includes(:question).each  { |a| add_answer(a) }
     end
+
 
     def find_answer(question)
       type = answer_type(question)
       return question if type.nil?
-      conditions = { :run => self.run, :question => question }
-      # If this is an ImageQuestion with an author-defined background image, we want to copy that into the answer.
-      if type == Embeddable::ImageQuestionAnswer and !question.is_shutterbug? and !question.bg_url.blank?
-        conditions[:image_url] = question.bg_url
+      answer =  self.answer_map[self.question_key(question)] 
+      if answer.nil?
+        conditions = { :run => self.run, :question => question }
+        # If this is an ImageQuestion with an author-defined background image, we want to copy that into the answer.
+        if type == Embeddable::ImageQuestionAnswer and !question.is_shutterbug? and !question.bg_url.blank?
+          conditions[:image_url] = question.bg_url
+        end
+        answer = type.create(conditions)
+        self.add_answer(answer)
       end
-      type.by_run(run).by_question(question).first || type.create(conditions)
+      return answer
+      # type.by_run(run).by_question(question).first || type.create(conditions)
+
     end
 
     def answer_type(question)
