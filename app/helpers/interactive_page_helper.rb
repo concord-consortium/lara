55882abcdc040b9fbd9153d3fdfbdbe63e3eff60
Noah Paessel <knowuh@gmail.com>
2014-10-09 16:58:02 -0400

    Don't put the current activity run ID's into the sequence navigation menu [#80388210]
    https://www.pivotaltracker.com/story/show/80388210

--- app/helpers/interactive_page_helper.rb
+++ app/helpers/interactive_page_helper.rb
@@ -1,7 +1,8 @@
 module InteractivePageHelper
   def runnable_activity_page_path(activity, page)
-    if @run
-      page_with_response_path(activity.id, page.id, @run.key)
+    run = run_for_activity(activity,@run)
+    if run
+      page_with_response_path(activity.id, page.id, run.key)
     elsif activity and page
       activity_page_path(activity, page)
     elsif activity
@@ -10,9 +11,19 @@ module InteractivePageHelper
       nil
     end
   end
+
   def page_link(activity,page, opts={})
     name = "Page #{page.position}"
     name = page.name unless page.name.blank?
     return link_to name, runnable_activity_page_path(activity,page), opts
+  end
+
+  protected
+  def run_for_activity(activity,run)
+    return nil unless run
+    return run if (run.activity == activity)
+    sequence_run = run.sequence_run
+    return nil unless sequence_run
+    return sequence_run.run_for_activity(activity)
   end
 end
---
+++ spec/helpers/interactive_page_helper_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+
+describe InteractivePageHelper do
+
+  let(:act_stubs)  {{}}
+  let(:page_stubs) {{}}
+  let(:sequence_run_stubs) {{}}
+
+  let(:activity)     { mock_model(LightweightActivity, act_stubs) }
+  let(:activity_2)   { mock_model(LightweightActivity, act_stubs) }
+  let(:page)         { mock_model(InteractivePage, page_stubs)    }
+  let(:run)          { mock_model(Run, run_stubs)                 }
+  let(:other_run)    { mock_model(Run, other_run_stubs)           }
+  let(:sequence_run) { nil }
+
+  let(:run_stubs) do
+    {key: "x" * 36,
+     activity: activity,
+     sequence_run: sequence_run
+    }
+  end
+
+  let(:other_run_stubs) do
+    {key: "y" * 36,
+     activity: activity_2
+    }
+  end
+  subject {helper.runnable_activity_page_path(activity,page)}
+
+  describe "#runnable_activity_page_path(activity, page)" do
+    describe "without a run" do
+      it "should do match a run-free path" do
+        subject.should == "/activities/#{activity.id}/pages/#{page.id}"
+      end
+    end
+
+    describe "with a run" do
+      before(:each) do
+        assign(:run, run)
+      end
+      describe "when the run isn't part of a sequence" do
+        it "the link should include a run's key" do
+          subject.should == "/activities/#{activity.id}/pages/#{page.id}/#{run.key}"
+        end
+
+        describe "when the run is for a different activity" do
+          subject {helper.runnable_activity_page_path(activity_2,page)}
+          it " the link should not include the run key" do
+            subject.should == "/activities/#{activity_2.id}/pages/#{page.id}" 
+          end
+        end
+      end
+
+      describe "when the run belongs to a sequence run" do
+        let(:sequence_run) { mock_model(SequenceRun, sequence_run_stubs)}
+        it "the link should include the run's key" do
+          subject.should == "/activities/#{activity.id}/pages/#{page.id}/#{run.key}"
+        end
+
+        describe "when the activity doesn't belong to the run" do
+          let(:sequence_run_stubs) {{run_for_activity: nil}}
+          subject {helper.runnable_activity_page_path(activity_2,page)}
+          it " the link should not include the run key" do
+            subject.should == "/activities/#{activity_2.id}/pages/#{page.id}" 
+          end
+          
+          describe "but if it is part of the same sequence" do
+            let(:sequence_run_stubs) {{run_for_activity: other_run}}
+            it " the link should include the other run's key" do
+              subject.should == "/activities/#{activity_2.id}/pages/#{page.id}/#{other_run.key}"  
+            end
+          end
+        end
+      end
+    end
+  end
+
+end
