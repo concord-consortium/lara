15c81ca128094f9b79ea80fc3196eb9902dca1fb
Noah Paessel <knowuh@gmail.com>
2014-10-22 16:39:42 -0400

    Fix some failing travis CI tests: [#81223932]
    https://www.pivotaltracker.com/story/show/81223932

--- spec/features/activity_run_from_portal_spec.rb
+++ spec/features/activity_run_from_portal_spec.rb
@@ -1,24 +1,27 @@
 require 'spec_helper'
 require 'uri'
 
-feature "Activity is run from the portal", :js => true, :slow => true do
+feature "Activity is run from the portal", :js => true do
+  # need an activity with at least a open response question
+  let(:activity)   { FactoryGirl.create(:activity_with_page_and_or) }
+  let(:user)       { FactoryGirl.create(:user_with_authentication)  }
+  let(:question)   { activity.questions.find {|q| q.class == Embeddable::OpenResponse} }
+  let(:question_id){ question.id }
+  let(:url) do
+    activity_path(activity, :params => {
+       :domain     => 'http://portal.org/',
+       :externalId => '1234',
+       :returnUrl  => 'http://portal.org/return/1234'
+      })
+  end
 
-  scenario "with a domain, external_id, and return url" do
-    # need an activity with at least a open response question
-  	activity = FactoryGirl.create(:activity_with_page_and_or)
-    user     = FactoryGirl.create(:user_with_authentication)
-
-    url = activity_path(activity, :params => {
-		     :domain     => 'http://portal.org/',
-		     :externalId => '1234',
-		     :returnUrl  => 'http://portal.org/return/1234'
-		     })
+  scenario "with a domain, external_id, and return url" do  
 
     visit url
     click_button "Begin activity"
 
     stub = stub_request(:post, "http://portal.org/return/1234").
-	  with(:body => "[{\"type\":\"open_response\",\"question_id\":\"1\",\"answer\":\"something\",\"is_final\":false}]",
+	  with(:body => "[{\"type\":\"open_response\",\"question_id\":\"#{question_id}\",\"answer\":\"something\",\"is_final\":false}]",
 	       :headers => {'Authorization'=>'Bearer token', 'Content-Type'=>'application/json'}).
 	  to_return(:status => 200, :body => "", :headers => {})
 
@@ -29,5 +32,3 @@ feature "Activity is run from the portal", :js => true, :slow => true do
 	stub.should have_been_requested
   end
 end
-
-
