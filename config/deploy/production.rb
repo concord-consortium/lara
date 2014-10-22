54c9fe0510db141864f16fdc34dcdbe503060d68
Noah Paessel <knowuh@gmail.com>
2014-10-22 18:54:52 -0400

    Deploy production to authoring.production now that authoring.concord is Load B.
    [#80762428]
    https://www.pivotaltracker.com/story/show/80762428

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :branch, "production"
-set :domain, "authoring.concord.org"
+set :domain, "authoring.production.concord.org"
 
 server domain, :app, :web
 role :db, domain, :primary => true
 
 set(:autoscaling_instance_type, "c1.medium")
-set(:autoscaling_security_groups, %w(lara))
+set(:autoscaling_security_groups, %w(lwmw))
 set(:autoscaling_min_size, 2)
 set(:autoscaling_max_size, 10)
 set(:autoscaling_application, 'LaraProdLB')
