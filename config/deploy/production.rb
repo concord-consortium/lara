bfe82800563992861d8269183864a4135917eb27
Noah Paessel <knowuh@gmail.com>
2014-11-07 17:20:57 -0500

    Update EC instance types to C3.large

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :domain, "authoring.production.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
 
-set(:autoscaling_instance_type, "c1.medium")
+set(:autoscaling_instance_type, "c3.large")
 set(:autoscaling_security_groups, %w(lwmw))
 set(:autoscaling_min_size, 1)
 set(:autoscaling_max_size, 6)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,9 @@ set :domain, "authoring.staging.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
 
-set(:autoscaling_instance_type, "c1.medium")
+set(:autoscaling_instance_type, "c3.large")
+set(:autoscaling_min_size, 1)
+set(:autoscaling_max_size, 6)
 set(:autoscaling_application, 'LaraStagingLB')
 set(:autoscaling_require_keys, true)
 set(:autoscaling_image_master_name, 'lara-staging') # will use 'stage' otherwise
