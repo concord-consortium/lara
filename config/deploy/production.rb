217b6209a3765852db0064b59c31eb735a437488
Noah Paessel <knowuh@gmail.com>
2014-11-18 10:01:44 -0500

    Remove autoscaling configuration from lara-staging.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,11 @@ set :domain, "authoring.production.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
 
+set(:autoscaling_create_image, true)
+set(:autoscaling_create_group, true)
+set(:autoscaling_create_policy, true)
+set(:autoscaling_create_launch_configuration, true)
+
 set(:autoscaling_instance_type, "c3.large")
 set(:autoscaling_security_groups, %w(lwmw))
 set(:autoscaling_min_size, 1)
