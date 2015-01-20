59f9e676458a09f7eb5885014202e0c24b84e724
Piotr Janik <janikpiotrek@gmail.com>
2015-01-20 15:34:16 +0100

    Remove unused ENV variables form sample file
    [#86512948]
    
    I guess they were used by Shutterbug

--- config/app_environment_variables.sample.rb
+++ config/app_environment_variables.sample.rb
@@ -8,14 +8,11 @@ ENV['CONCORD_HAS_STAGING_CLIENT_ID']     ||= 'localhost'
 ENV['CONCORD_HAS_STAGING_CLIENT_SECRET'] ||= 'XXXXXX'
 ENV['CONCORD_HAS_STAGING_DISPLAY_NAME']  ||= 'Has Staging Portal'  # Optional param
 
+ENV['SECRET_TOKEN']                      ||= 'use `rake secret` to generate'
+
 # Decides whether SVG Edit ('svg-edit' or undefined) or CC Drawing Tool ('drawing-tool') should be used.
 ENV['CONCORD_DRAWING_TOOL']              ||= 'drawing-tool'
-
-ENV['SECRET_TOKEN']                      ||= 'use `rake secret` to generate'
-ENV['S3_KEY']                            ||= 'xxxxxxxxxxxxx'
-ENV['S3_SECRET']                         ||= 'xxxxxxxxxxxxx'
-ENV['S3_BIN']                            ||= 'ccshutterbug'
 ENV['SHUTTERBUG_URI']                    ||= "//snapshot.concord.org/shutterbug"
-
 ENV['LOGGER_URI']                        ||= '//cc-log-manager.herokuapp.com/api/logs'
 ENV['LOGGER_APPLICATION_NAME']           ||= 'LARA-log-poc'
+
