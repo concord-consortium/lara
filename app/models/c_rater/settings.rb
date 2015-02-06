7b43e6385ef194bedb593edd4b28ecce26f9eda5
Piotr Janik <janikpiotrek@gmail.com>
2015-02-06 23:51:01 +0100

    Add C-Rater settings controller (edit / update settings)
    [#86636630]

--- app/models/c_rater/settings.rb
+++ app/models/c_rater/settings.rb
@@ -1,5 +1,5 @@
 class CRater::Settings < ActiveRecord::Base
   belongs_to :provider, polymorphic: true
   belongs_to :score_mapping
-  attr_accessible :item_id
+  attr_accessible :item_id, :score_mapping_id
 end
