895052ca061648025569ecb1cf533ec2c934754e
Piotr Janik <janikpiotrek@gmail.com>
2015-02-09 17:44:32 +0100

    Display argumentation block in runtime
    [#87949862]

--- app/helpers/c_rater/argumentation_block_helper.rb
+++ app/helpers/c_rater/argumentation_block_helper.rb
@@ -1,10 +1,17 @@
-module CRater::AuthoringHelper
+module CRater::ArgumentationBlockHelper
 
+  # Used during authoring.
   def arg_block_authorables(page)
     embeddables = page.section_embeddables(CRater::ARG_SECTION_NAME)
     # Return list of embeddables + C-Rater settings if available.
     embeddables.map { |e|
       e.respond_to?(:c_rater_settings) && e.c_rater_settings ? [e, e.c_rater_settings] : e
     }.flatten
+  end
+
+  # Used in runtime.
+  def arg_block_embeddables(page, run)
+    finder = Embeddable::AnswerFinder.new(run)
+    page.section_embeddables(CRater::ARG_SECTION_NAME).map { |e| finder.find_answer(e) }
   end
 end
