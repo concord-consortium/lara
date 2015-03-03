e2118cd8b0d0f44c4d5a7f1d5f7f56bea1ef4373
Sagar Talla <sagar.talla@zeuslearning.com>
2015-03-03 17:43:39 +0530

    Fix indentation.

--- app/models/c_rater/settings_provider_functionality.rb
+++ app/models/c_rater/settings_provider_functionality.rb
@@ -8,33 +8,33 @@ module CRater::SettingsProviderFunctionality
     base.alias_method_chain :export, :c_rater
     
     class << base
-    	def import_with_c_rater(import_hash)
-    		import_embeddable = self.import_without_c_rater(import_hash.except(:item_settings))
-		 		if import_hash[:item_settings]
-		      item_settings = CRater::ItemSettings.import(import_hash[:item_settings])
-		      item_settings.provider = import_embeddable
-		      item_settings.save!(validate: false)
-		    end
-		    import_embeddable
-    	end
-    	alias_method_chain :import, :c_rater
-  	end
-  	
+      def import_with_c_rater(import_hash)
+        import_embeddable = self.import_without_c_rater(import_hash.except(:item_settings))
+        if import_hash[:item_settings]
+          item_settings = CRater::ItemSettings.import(import_hash[:item_settings])
+          item_settings.provider = import_embeddable
+          item_settings.save!(validate: false)
+        end
+        import_embeddable
+      end
+      alias_method_chain :import, :c_rater
+    end
+    
   end
   
   def duplicate_with_c_rater
-  	copy = duplicate_without_c_rater
-  	if self.c_rater_item_settings
-  		item_settings = self.c_rater_item_settings.duplicate
-    	item_settings.provider = copy
-    	item_settings.save!(validate: false)
-  	end
+    copy = duplicate_without_c_rater
+    if self.c_rater_item_settings
+      item_settings = self.c_rater_item_settings.duplicate
+      item_settings.provider = copy
+      item_settings.save!(validate: false)
+    end
     copy
   end
  
   def export_with_c_rater
-  	export_json = export_without_c_rater
-  	if self.c_rater_item_settings
+    export_json = export_without_c_rater
+    if self.c_rater_item_settings
         export_json[:item_settings] = self.c_rater_item_settings.export
     end
     export_json
