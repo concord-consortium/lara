f5ce69ec3a321093a266d3952f36b6b106b63367
Aaron Unger <aaron.unger@gmail.com>
2015-04-14 13:06:50 -0700

    Fix tests for new strict Run access controls

--- spec/controllers/embeddable/labbook_answers_controller_spec.rb
+++ spec/controllers/embeddable/labbook_answers_controller_spec.rb
@@ -8,22 +8,47 @@ describe Embeddable::LabbookAnswersController do
   let(:question) { Embeddable::Labbook.create! }
   let(:answer)   { Embeddable::LabbookAnswer.create!(question: question, run: run) }
   let(:endpoint) { 'http://concord.portal.org' }
+  let(:user)     { FactoryGirl.create(:user) }
 
   describe '#update' do
     describe 'with a run initiated from remote portal' do
-      let(:run) { FactoryGirl.create(:run, remote_endpoint: endpoint) }
-      it 'should fire off a web request to update the portal' do
-        put 'update', id: answer.id
-        assert_requested :post, endpoint
+      describe 'and logged in' do
+        let(:run) { FactoryGirl.create(:run, remote_endpoint: endpoint, user_id: user.id) }
+        it 'should fire off a web request to update the portal' do
+          sign_in user
+          put 'update', id: answer.id
+          expect(response.status).not_to eq 401
+          assert_requested :post, endpoint
+        end
+      end
+      describe 'and anonymous' do
+        let(:run) { FactoryGirl.create(:run, remote_endpoint: endpoint, user_id: nil) }
+        it 'should fire off a web request to update the portal' do
+          put 'update', id: answer.id
+          expect(response.status).not_to eq 401
+          assert_requested :post, endpoint
+        end
       end
     end
   end
 
   describe 'with no endpoint defined (not requested from portal)' do
-    let(:run) { FactoryGirl.create(:run) }
-    it 'should not fire a web request to update the portal' do
-      put 'update', id: answer.id
-      assert_not_requested :post, endpoint
+    describe 'and logged in' do
+      let(:run) { FactoryGirl.create(:run, user_id: user.id) }
+      it 'should not fire a web request to update the portal' do
+        sign_in user
+        put 'update', id: answer.id
+        expect(response.status).not_to eq 401
+        assert_not_requested :post, endpoint
+      end
+    end
+    describe 'and anonymous' do
+      let(:run) { FactoryGirl.create(:run, user_id: nil) }
+      it 'should not fire a web request to update the portal' do
+        put 'update', id: answer.id
+        expect(response.status).not_to eq 401
+        assert_not_requested :post, endpoint
+      end
     end
   end
 end
