da2e898d4360b4dcdc765e9353d09303e65d5af7
Scott Cytacki <scytacki@concord.org>
2015-06-15 12:07:25 -0400

    add license info to bind-polyfill

--- app/assets/javascripts/bind-polyfill.js
+++ app/assets/javascripts/bind-polyfill.js
@@ -1,3 +1,6 @@
+// Any copyright is dedicated to the Public Domain. http://creativecommons.org/publicdomain/zero/1.0/
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Polyfill
+// license info: https://developer.mozilla.org/en-US/docs/MDN/About#Copyrights_and_licenses
 if (!Function.prototype.bind) {
   Function.prototype.bind = function(oThis) {
     if (typeof this !== 'function') {
