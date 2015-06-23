7f9bf6add920bc9cdd0a6acec8e4133d2685f68d
Doug Martin <doug@dougmart.in>
2015-06-23 11:03:57 -0500

    Fixed single and multi-page show to exclude hidden pages [#95538134]

--- spec/views/lightweight_activities/show.html.haml_spec.rb
+++ spec/views/lightweight_activities/show.html.haml_spec.rb
@@ -4,8 +4,8 @@ describe 'lightweight_activities/show' do
   # Stub what we need to render the page
   let (:page1) { stub_model(InteractivePage, :id => 1, :name => Faker::Lorem.sentence(1)[0..20]) }
   let (:page2) { stub_model(InteractivePage, :id => 2, :name => Faker::Lorem.sentence(1)[0..20]) }
-  let (:activity) { stub_model(LightweightActivity, :id => 1, :name => Faker::Lorem.sentence(2)[0..49], :description => Faker::Lorem.sentences(3).join(" "), :pages => [page1, page2]) }
-  
+  let (:activity) { stub_model(LightweightActivity, :id => 1, :name => Faker::Lorem.sentence(2)[0..49], :description => Faker::Lorem.sentences(3).join(" "), :visible_pages => [page1, page2]) }
+
   # Assigns
   before(:each) do
     assign(:activity, activity)
@@ -24,7 +24,7 @@ describe 'lightweight_activities/show' do
 
   it 'has numbered list of pages with titles' do
     render
-    expect(rendered).to have_css '.intro-mod ol li', :count => activity.pages.length
+    expect(rendered).to have_css '.intro-mod ol li', :count => activity.visible_pages.length
     expect(rendered).to match /#{page1.name}/
     expect(rendered).to match /#{page2.name}/
   end
