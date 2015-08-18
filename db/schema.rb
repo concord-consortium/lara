bfac4dc4d2f80409d713a590a2a5914a72468e5f
Scott Cytacki <scytacki@concord.org>
2015-08-18 11:37:44 -0400

    oops, missed update version header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150807175612) do
+ActiveRecord::Schema.define(:version => 20150818153040) do
 
   create_table "admin_events", :force => true do |t|
     t.string   "kind"
