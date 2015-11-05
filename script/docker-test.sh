7bc83f0f96c1a1933e50e1d1b5a89fc67df09ec0
Noah Paessel <knowuh@gmail.com>
2015-11-05 18:08:44 -0500

    Update documentation

--- script/docker-test.sh
+++ script/docker-test.sh
@@ -2,7 +2,9 @@
 
 # If guard doesn't trigger, try running vagrant rsync-auto
 export RAILS_ENV=test
+export DB_NAME=lara_test
 bundle exec spring rake db:create
+bundle exec spring rake db:migrate
 bundle exec spring rake db:test:prepare
 # -p flag needed here because of a bug / misfeature in guard, for more info:
 # https://groups.google.com/forum/#!topic/guard-dev/-tx6yncq1wA
