52c21fce77178e148942a2416d8befc79702d1f5
Piotr Janik <janikpiotrek@gmail.com>
2016-01-22 17:11:38 -0800

    Remove dashboard runs filtering
    [#111245000]
    [#111715140]
    
    + provide group_id

--- app/services/dashboard_runlist.rb
+++ app/services/dashboard_runlist.rb
@@ -1,17 +1,10 @@
 class DashboardRunlist
-  # If remote endpoint includes `key:` prefix, it means it's using long UUID key
-  # instead of numeric ID, e.g.:
-  # https://learn.concord.org/dataservice/external_activity_data/key:19679d16-e79e-4f36-8253-5deb1321e6d9
-  SECURE_ENDPOINT_URL_REGEXP = /key:/
-
   def initialize(endpoint_urls, page_id)
-    # Make sure that we accept only endpoints that use UUID to identify learner.
-    # If we accepted numeric ID, it would be easy to modify it and download other students data.
-    endpoint_urls = filter_endpoints(endpoint_urls)
     @runs = Run.where(remote_endpoint: endpoint_urls).map do |run|
       {
         endpoint_url: run.remote_endpoint,
-        last_page_id: run.last_page ? run.last_page.id : nil,
+        group_id: run.collaboration_run_id,
+        last_page_id: run.page_id,
         submissions: submissions(run, page_id)
       }
     end
@@ -27,16 +20,11 @@ class DashboardRunlist
 
   private
 
-  def filter_endpoints(endpoints)
-    endpoints.select do |endpoint|
-      endpoint =~ SECURE_ENDPOINT_URL_REGEXP
-    end
-  end
-
   def submissions(run, page_id)
     CRater::FeedbackSubmission.where(run_id: run.id, interactive_page_id: page_id).order(:id).map do |submission|
       {
         id: submission.id,
+        group_id: submission.collaboration_run_id,
         answers: answers(submission)
       }
     end
