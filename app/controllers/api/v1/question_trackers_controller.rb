643461bff4ba499523fd19d38e4942f2aa067d77
Noah Paessel <knowuh@gmail.com>
2016-05-26 05:22:42 -0400

    QuestionTracker listings return report_urls

--- app/controllers/api/v1/question_trackers_controller.rb
+++ app/controllers/api/v1/question_trackers_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::QuestionTrackersController < ApplicationController
 
   def index
     list = QuestionTracker::Reporter.list.map {|tracker| QuestionTracker::Reporter.tracker_json(tracker)}
-    render :json => list
+    render :json => list.map {|tracker| add_report_url(tracker)}
   end
 
   def find_by_activity
     activity = LightweightActivity.find(params[:activity_id])
     list = QuestionTracker::Reporter.question_trackers_for_activity(activity).map do |tracker|
-      QuestionTracker::Reporter.tracker_json(tracker)
+      add_report_url(QuestionTracker::Reporter.tracker_json(tracker))
     end
     render :json => list
   end
@@ -16,19 +16,23 @@ class Api::V1::QuestionTrackersController < ApplicationController
   def find_by_sequence
     sequence = Sequence.find(params[:sequence_id])
     list = QuestionTracker::Reporter.question_trackers_for_sequence(sequence).map do |tracker|
-      QuestionTracker::Reporter.tracker_json(tracker)
+      add_report_url(QuestionTracker::Reporter.tracker_json(tracker))
     end
-    render :json => list
+    render :json => list.flatten
   end
 
   def report
-    # Right now only admins can get here.
-    authorize! :report, QuestionTracker
     tracker = QuestionTracker.find(params[:question_tracker_id])
-    reporter = QuestionTracker::Reporter.new(tracker)
+    reporter = QuestionTracker::Reporter.new(tracker, params[:learner_json])
     render :json => {
         "question_tracker" => reporter.report_info,
         "answers" => reporter.answers
     }
   end
+
+  private
+  def add_report_url(trackerHash)
+    trackerHash.merge(report_url:api_v1_question_tracker_report_url(trackerHash[:id]))
+  end
+
 end
