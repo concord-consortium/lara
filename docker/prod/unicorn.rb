450c39a478fe90d47f1ca646bf7f657ecfab0fde
Scott Cytacki <scytacki@concord.org>
2016-05-26 12:41:00 -0400

    increase unicorn timeout, and make configurable

--- docker/prod/unicorn.rb
+++ docker/prod/unicorn.rb
@@ -6,4 +6,4 @@ pid "#{app_dir}/tmp/unicorn.pid"
 
 worker_processes (ENV['NUM_UNICORN_PROCESSES'] || 1).to_i
 listen "/tmp/unicorn.sock", :backlog => 64
-timeout 30
+timeout (ENV['UNICORN_TIMEOUT'] || 90).to_i
