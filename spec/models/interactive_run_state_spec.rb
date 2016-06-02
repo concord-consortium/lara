66e805ccb13757da92f1afc88371902b7fb9ae2d
Noah Paessel <knowuh@gmail.com>
2016-06-02 17:31:20 -0400

    Test for linked_state in InteractiveRunState.
    [#118940579]

--- spec/models/interactive_run_state_spec.rb
+++ spec/models/interactive_run_state_spec.rb
@@ -6,12 +6,7 @@ describe InteractiveRunState do
   let(:activity)        { FactoryGirl.create(:activity)       }
   let(:interactive)     { FactoryGirl.create(:mw_interactive) }
   let(:user)            { FactoryGirl.create(:user)           }
-  let(:run) do
-    r = FactoryGirl.create(:run)
-    r.activity = activity
-    r.user = user
-    r
-  end
+  let(:run)             { FactoryGirl.create(:run, {activity: activity, user: user})}
 
   describe 'class methods' do
     describe "InteractiveRunState#by_run_and_interactive" do
@@ -38,7 +33,35 @@ describe InteractiveRunState do
     end
   end
 
-  describe 'instance methods' do
-  end
+  describe "instance methods" do
+    describe "to_runtime_json" do
+      let(:run_data) {'{"second": 2}"'}
+      let(:interactive_run_state) { InteractiveRunState.create(run: run, interactive: interactive, raw_data: run_data)}
+      let(:result_hash) { JSON.parse(interactive_run_state.to_runtime_json) }
+      describe "when the interactice run state has no parent" do
+        it "should return the raw_data" do
+          expect(result_hash["raw_data"]).to eql run_data
+        end
+        it "should not have any linked state" do
+          expect(result_hash["linked_state"]).to be_nil
+        end
+      end
+      describe "when the interactive run state has a parent" do
+        let(:parent_run_data)    {'{"first": 1}"'}
+        let(:parent_interactive) { FactoryGirl.create(:mw_interactive)}
+        let(:parent_run_state)   { InteractiveRunState.create(run: run, interactive: parent_interactive, raw_data: parent_run_data)}
+        let(:interactive)        { FactoryGirl.create(:mw_interactive, {parent_id: parent_interactive.id})}
+        describe "when the interactice run state has no parent" do
+          it "should return the raw_data" do
+            expect(result_hash["raw_data"]).to eql run_data
+          end
+          it "should also include linked state" do
+            make parent_run_state
+            expect(result_hash["linked_state"]).to eql parent_run_data
+          end
+        end
+      end
+    end
 
+  end
 end
