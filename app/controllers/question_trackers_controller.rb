26cec1dd43f5bbb60d0e9807a834c6076a1d690c
Scott Cytacki <scytacki@concord.org>
2016-06-13 22:55:44 -0400

    simplify question_trackers_controller
    this uses the built in cancancan load_and_authorize method

--- app/controllers/question_trackers_controller.rb
+++ app/controllers/question_trackers_controller.rb
@@ -1,40 +1,38 @@
 # encoding: UTF-8
 
 class QuestionTrackersController < ApplicationController
-  before_filter :set_question_tracker, except: [:new, :index, :create]
+  load_and_authorize_resource
   before_filter :record_qt_origin
 
   def index
-    authorize! :manage, QuestionTracker
     @question_trackers = QuestionTracker.all # TODO Limit by user.
   end
 
   def new
-    authorize! :manage, QuestionTracker
     mc = Embeddable::MultipleChoice.create
     mc.create_default_choices
     @question_tracker = QuestionTracker.create(master_question: mc, name: "My question tracker", description: "for …")
-    gon.QuestionTracker = QuestionTracker::Editor.new(@question_tracker).to_json
-    gon.DoneLink = session[:qt_origin] || question_trackers_path
+    prepare_gon(@question_tracker)
     render :edit
   end
 
   def show
     authorize! :manage, QuestionTracker
     respond_to do |format|
-      format.html { }
+      format.html {
+        prepare_gon(@question_tracker)
+      }
       format.json { render json: QuestionTracker::Editor.new(@question_tracker).to_json  }
     end
   end
 
   def edit
-    authorize! :manage, QuestionTracker
+    prepare_gon(@question_tracker)
     # renders edit form for @question_tracker
   end
 
 
   def update
-    authorize! :manage, QuestionTracker
     respond_to do |format|
       # no support for html.
       format.json do
@@ -46,7 +44,6 @@ class QuestionTrackersController < ApplicationController
   end
 
   def destroy
-    authorize! :manage, QuestionTracker
     @question_tracker.destroy
     redirect_or_index
   end
@@ -54,11 +51,10 @@ class QuestionTrackersController < ApplicationController
 
   private
 
-  def set_question_tracker
-    @question_tracker = QuestionTracker.find(params[:id])
+  def prepare_gon(question_tracker)
     # Data assigned to `gon` variable will be available for JavaScript code in `window.gon` object.
     # this is used in the Question Tracker editor react views.
-    gon.QuestionTracker = QuestionTracker::Editor.new(@question_tracker).to_json
+    gon.QuestionTracker = QuestionTracker::Editor.new(question_tracker).to_json
     gon.DoneLink = session[:qt_origin] || question_trackers_path
   end
 
