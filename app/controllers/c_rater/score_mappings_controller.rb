fb1473ca1d57ab13014f55eda86023f07a039130
Piotr Janik <janikpiotrek@gmail.com>
2016-06-15 12:03:56 +0200

    Simplify ScoreMappingsController
    [#120788853]

--- app/controllers/c_rater/score_mappings_controller.rb
+++ app/controllers/c_rater/score_mappings_controller.rb
@@ -1,18 +1,12 @@
 class CRater::ScoreMappingsController < ApplicationController
+  authorize_resource
 
   def index
-    if current_user && current_user.admin?
-      @filter = CollectionFilter.new(current_user, CRater::ScoreMapping, params[:filter] || {})
-      @score_mappings = @filter.collection.includes(:user)
-    else
-      response_data = unauthorized_response_data("no_action", "no_resource")
-      render :partial => "shared/unauthorized", :locals => {:message => response_data[:message]}, :status => 403
-      return
-    end
+    @filter = CollectionFilter.new(current_user, CRater::ScoreMapping, params[:filter] || {})
+    @score_mappings = @filter.collection.includes(:user)
   end
 
   def new
-    authorize! :manage, CRater::ScoreMapping
     @score_mapping = CRater::ScoreMapping.new()
     respond_to do |format|
       format.js { render :json => { :html => render_to_string('new'), :css_class => "feedback-set" }, :content_type => 'text/json' }
@@ -21,8 +15,7 @@ class CRater::ScoreMappingsController < ApplicationController
   end
 
   def create
-    authorize! :manage, CRater::ScoreMapping
-    score_mapping = { mapping: params[:c_rater_score_mapping].slice(:score0,:score1,:score2,:score3,:score4,:score5,:score6)}
+    score_mapping = {mapping: params[:c_rater_score_mapping].slice(:score0, :score1, :score2, :score3, :score4, :score5, :score6)}
     @score_mapping = CRater::ScoreMapping.create(score_mapping)
     @score_mapping.description = params[:c_rater_score_mapping][:description]
     @score_mapping.user = current_user
@@ -32,7 +25,6 @@ class CRater::ScoreMappingsController < ApplicationController
   end
 
   def edit
-    authorize! :manage, CRater::ScoreMapping
     @score_mapping = CRater::ScoreMapping.find(params[:id])
     respond_to do |format|
       format.js { render :json => { :html => render_to_string('edit'), :css_class => "feedback-set"}, :content_type => 'text/json' }
@@ -41,7 +33,6 @@ class CRater::ScoreMappingsController < ApplicationController
   end
 
   def update
-    authorize! :manage, CRater::ScoreMapping
     @score_mapping = CRater::ScoreMapping.find(params[:id])
     score_mapping = { mapping: params[:c_rater_score_mapping].slice(:score0,:score1,:score2,:score3,:score4,:score5,:score6)}
     @score_mapping.description = params[:c_rater_score_mapping][:description]
@@ -52,7 +43,6 @@ class CRater::ScoreMappingsController < ApplicationController
   end
 
   def destroy
-    authorize! :manage, CRater::ScoreMapping
     @score_mapping = CRater::ScoreMapping.find(params[:id])
     @score_mapping.destroy
     redirect_to(:back)
