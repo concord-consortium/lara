813d9dba303b7242f23cf1b55e15121dbb25d5d8
Piotr Janik <janikpiotrek@gmail.com>
2016-06-19 16:58:25 +0200

    Optimize Dashboard API (reduce N+1 queries, support update timestamps)
    [#120872571]

--- app/services/dashboard_runlist.rb
+++ app/services/dashboard_runlist.rb
@@ -1,21 +1,32 @@
 class DashboardRunlist
-  def initialize(endpoint_urls, page_id)
-    @runs = Run.includes(:activity).where(remote_endpoint: endpoint_urls).map do |run|
+  DATA_VERSION = 2
+
+  def initialize(endpoint_urls, page_id, submissions_created_after = 0)
+    # DB query can be optimized by filtering out submissions older
+    # than provided timestamp. If it's not provided (or nil), we will simply return all the submissions.
+    submissions_created_after = Time.at(submissions_created_after ? submissions_created_after.to_i : 0)
+    runs_data = Run.includes(:activity).where(remote_endpoint: endpoint_urls).map do |run|
       {
         endpoint_url: run.remote_endpoint,
         group_id: run.collaboration_run_id,
         last_page_id: run.page_id,
-        submissions: submissions(run, page_id),
+        submissions: submissions(run, page_id, submissions_created_after),
         sequence_id: run.sequence_id,
         updated_at: run.updated_at.to_i,
         activity_id: run.activity_id,
         page_ids: run.activity.page_ids
       }
     end
+    @data = {
+      runs: runs_data,
+      # Timestamp can be used by the client to provide submissions_created_after param in the subsequent requests.
+      timestamp: Time.now.to_i,
+      version: DATA_VERSION
+    }
   end
 
   def to_hash
-    @runs
+    @data
   end
 
   def to_json
@@ -24,8 +35,19 @@ class DashboardRunlist
 
   private
 
-  def submissions(run, page_id)
-    CRater::FeedbackSubmission.includes(:c_rater_feedback_items => :answer).where(run_id: run.id, interactive_page_id: page_id).order(:id).map do |submission|
+  def submissions(run, page_id, submissions_created_after)
+    CRater::FeedbackSubmission
+      .includes(
+        c_rater_feedback_items: {answer: [:run, :question]},
+        embeddable_feedback_items: {answer: [:run, :question]}
+      )
+      .where('created_at > ?', submissions_created_after)
+      .where(
+        run_id: run.id,
+        interactive_page_id: page_id
+      )
+      .order(:id)
+      .map do |submission|
       {
         id: submission.id,
         group_id: submission.collaboration_run_id,
