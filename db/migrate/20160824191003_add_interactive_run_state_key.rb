23032de77182e4016e0accfd3e796b70f012dc75
Scott Cytacki <scytacki@concord.org>
2016-09-08 23:29:35 -0400

    use find_each so we don’t run out memory
    this change was made on production in order to run the migration on
    during the 1.12.0 deploy.

--- db/migrate/20160824191003_add_interactive_run_state_key.rb
+++ db/migrate/20160824191003_add_interactive_run_state_key.rb
@@ -4,7 +4,7 @@ class AddInteractiveRunStateKey < ActiveRecord::Migration
     add_index :interactive_run_states, :key, :name => 'interactive_run_states_key_idx'
 
     # create random key for the existing run states
-    InteractiveRunState.all.each do |interactive_run_state|
+    InteractiveRunState.find_each do |interactive_run_state|
       interactive_run_state.key = InteractiveRunState.generate_key
       interactive_run_state.save
     end
