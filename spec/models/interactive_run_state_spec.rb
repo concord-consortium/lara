fc15ef6dba0df8159af2f1898b8f21ed648237fe
Piotr Janik <janikpiotrek@gmail.com>
2017-03-12 22:51:43 +0100

    Update specs
    [#134801765]

--- spec/models/interactive_run_state_spec.rb
+++ spec/models/interactive_run_state_spec.rb
@@ -106,14 +106,14 @@ describe InteractiveRunState do
 
     # this hash is depended on by the Portal
     describe "portal_hash" do
-      let(:run_data) {'{"second": 2}"'}
+      # Only when reporting_url is available.
+      let(:run_data) {'{"second": 2, "lara_options": {"reporting_url": "test.com"}}'}
       let(:interactive_run_state) { InteractiveRunState.create(run: run, interactive: interactive, raw_data: run_data)}
 
       subject { interactive_run_state.portal_hash }
 
       # the portal uses this type to match the interactive so it can't change without changing the portal
       it { should include("question_type" => "iframe interactive") }
-
     end
 
     # this key is generated automatically when created
