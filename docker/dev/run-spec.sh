fd9777482c2e50e73f913aed30fefdc20d8521a1
Noah Paessel <knowuh@gmail.com>
2017-05-22 13:01:34 -0400

    Updates to documentation. Added docker testing scripts.

---
+++ docker/dev/run-spec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# use:  docker-compose run --rm app docker/dev/run-spec.sh
+# Run rspec tests in docker environment.
+#
+
+#
+# Prepare spec tests
+#
+RAILS_ENV=test bundle exec rake db:test:prepare
+
+#
+# Run non-phantom tests:
+#
+RAILS_ENV=test bundle exec rspec spec --tag ~slow --tag ~js
