2f7dfa161cf43e3a59dd332602a7500ad876a5fd
Noah Paessel <knowuh@gmail.com>
2017-06-28 12:43:24 -0400

    Spec tests wont clobber dev DB.  Unison prefers local changes.

--- docker/dev/run-spec.sh
+++ docker/dev/run-spec.sh
@@ -6,10 +6,17 @@
 #        then type `dspec` to start Continuous Integration Testing.
 #   – Or run from shell in docker (`docker-compose run --rm bash` … ./docker/dev/run-rpsec.sh`)
 
+# We have to explicitly set these, because the docker compose files
+# Assume master/master and mysql image wont let us create more than
+# one DB at startup.  master/master doesn't have create DB permissions.
+export RAILS_ENV=test
+export DB_NAME=lara_test
+export DB_USER=root
+export DB_PASSWORD=xyzzy
+
 #
 # Prepare spec tests
 #
-export RAILS_ENV=test
 bundle exec rake db:create
 bundle exec rake db:schema:load
 bundle exec rake db:test:prepare
