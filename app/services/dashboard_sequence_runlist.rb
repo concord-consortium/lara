b0843c1d11fc23c5cd29b286dbed6c52de4c2082
Noah Paessel <knowuh@gmail.com>
2017-07-06 11:12:42 -0400

    Fix typo sumbission → submission.

--- app/services/dashboard_sequence_runlist.rb
+++ app/services/dashboard_sequence_runlist.rb
@@ -81,15 +81,15 @@ class DashboardSequenceRunlist
       return []
     end
     submissions = submissions.group_by(&:interactive_page_id)
-    submissions.map do |page_id, page_sumbissions|
-      num_tries = page_sumbissions.length
-      page_sumbission = page_sumbissions.last
+    submissions.map do |page_id, page_submissions|
+      num_tries = page_submissions.length
+      page_submission = page_submissions.last
       {
         page: page_id,
-        pageIndex: page_sumbission.interactive_page.position,
+        pageIndex: page_submission.interactive_page.position,
         tryCount: num_tries,
         numQuestions: 4,
-        answers: submission_answers(page_sumbission)
+        answers: submission_answers(page_submission)
       }
     end
   end
