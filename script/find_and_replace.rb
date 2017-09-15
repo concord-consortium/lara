33c432009de950e1ae37aace1cd9dce0f83137ad
Scott Cytacki <scytacki@concord.org>
2017-09-14 21:06:37 -0400

    utility methods for find and replacing strings in LARA

---
+++ script/find_and_replace.rb
@@ -0,0 +1,118 @@
+text_fields = {
+  "InteractivePage" => [
+    :text,
+    :name,
+    :sidebar,
+    :sidebar_title
+  ],
+  "ImageInteractive" => [
+    :caption,
+    :credit
+  ],
+  "LightweightActivity" => [
+    :name,
+    :description,
+    :notes
+  ],
+  "MwInteractive" => [
+    :name
+  ],
+  "Sequence" => [
+    :description,
+    :title,
+    :display_title
+  ],
+  "VideoInteractive" => [
+    :caption,
+    :credit
+  ],
+  "Embeddable::Xhtml" => [
+    :name,
+    :content
+  ],
+  "Embeddable::OpenResponse" => [
+    :name,
+    :prompt,
+    :hint,
+    :prediction_feedback,
+    :default_text
+  ],
+  "Embeddable::MultipleChoice" => [
+    :name,
+    :prompt,
+    :hint,
+    :prediction_feedback
+  ],
+  "Embeddable::MultipleChoiceChoice" => [
+    :choice,
+    :prompt
+  ],
+  "Embeddable::Labbook" => [
+    :name,
+    :prompt,
+    :hint
+  ],
+  "Embeddable::ImageQuestion" => [
+    :name,
+    :prompt,
+    :hint,
+    :drawing_prompt,
+    :prediction_feedback
+  ]
+}
+
+def find_text(model_class, field_name, text)
+  model_class.constantize.where("#{field_name} like '%#{text}%'")
+end
+
+def replace_text(model, field_name, text, replacement)
+  old_text = model.send(field_name.to_s)
+  new_text = old_text.gsub(text, replacement)
+  model.send("#{field_name}=".to_s, new_text)
+  puts "+ replacing text of #{model.class.name}(#{model.id})##{field_name}"
+  begin
+    model.save!
+  rescue
+    puts "- failed to save #{model.class.name}(#{model.id})##{field_name}"
+  end
+end
+
+def print_results(results, field_name, text)
+  results.each do |model|
+    found_text = model.send(field_name.to_s)
+    highlighted = found_text.gsub(text,"\e[1m\\0\e[0m")
+    puts "------------------"
+    puts "#{model.class.name}(#{model.id})##{field_name} #{highlighted}"
+  end
+  nil
+end
+
+def find_and_print_all(text_fields, text)
+  total = 0
+  text_fields.each do |model_class, fields|
+    fields.each do |field_sym|
+      results = find_text(model_class, field_sym.to_s, text)
+      total+= results.count
+      print_results(results, field_sym.to_s, text)
+    end
+  end
+  puts "------------"
+  puts "Total Found: #{total}"
+end
+
+# https://s3.amazonaws.com/itsi-production/images-2009
+# find_and_replace_all(text_fields, "http://itsi.portal.concord.org/system/images", "https://s3.amazonaws.com/itsi-production/images-2009")
+def find_and_replace_all(text_fields, text, replacement)
+  total = 0
+  text_fields.each do |model_class, fields|
+    fields.each do |field_sym|
+      results = find_text(model_class, field_sym.to_s, text)
+      puts "Found #{results.count} in #{model_class}##{field_sym.to_s}"
+      total+= results.count
+      results.each do |model|
+        replace_text(model, field_sym.to_s, text, replacement)
+      end
+    end
+  end
+  puts "Total Found: #{total}"
+end
