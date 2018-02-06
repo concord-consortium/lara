4c7a309f638f85687b8d3397bf97933b7df79927
Noah Paessel <knowuh@gmail.com>
2018-02-06 14:19:24 -0500

    Changes to  registering and calling `ExternalScripts`:
    1. An external script registers itself via global access too `ExternalScripts.register('label', constructor)`
    2. The Embeddable ExternalScript element includes a javascript to invoke the constructor `ExternalScripts.init('label', {context})`

--- app/assets/javascripts/external-scripts.js
+++ app/assets/javascripts/external-scripts.js
@@ -1,23 +1,52 @@
-ExternalScripts = { _scripts: [] };
+ExternalScripts = {
+  _constructors: {},
+  _scripts:[],
+  _script_labels:[]
+};
 
-ExternalScripts.register = function(_constructor, env) {
-  try {
-    var config = {};
-    if (typeof env.config == 'string'){
-      config = JSON.parse(env.config);
-      delete env.config;
+ExternalScripts.notRegistered = function(label) {
+  console.error("No external script registered for " + label);
+};
+
+ExternalScripts.externalScriptError = function(e, other) {
+  console.group("External Script Error");
+  console.error(e);
+  console.dir(other);
+  console.groupEnd();
+};
+
+ExternalScripts.init = function(_label, ctx) {
+  var constructor = this._constructors[_label];
+  var config = {};
+  var script = null;
+  if (typeof constructor == 'function') {
+    if (typeof ctx.config == 'string'){
+      config = JSON.parse(ctx.config);
+      delete ctx.config;
     };
-    var script = new _constructor(config, env);
-    this._scripts.push(script);
-    console.group("ExternalScripts: Register");
-      console.dir(env);
-      console.dir(config);
-    console.groupEnd();
+    try {
+      script = new constructor(config, ctx);
+      this._scripts.push(script);
+      this._script_labels.push(_label);
+    }
+    catch(e) {
+      this.externalScriptError(e, ctx);
+    }
   }
-  catch(e) {
-    console.group("External Script Error");
-    console.error(e);
-    console.groupEnd();
+  else {
+    this.notRegistered(_label);
+  }
+}
+
+
+ExternalScripts.register = function(_label, _constructor) {
+  if(typeof _constructor == 'function') {
+    if(this._constructors[_label]) {
+      console.error("Duplicate script constructor for label " + _label);
+    }
+    else {
+      this._constructors[_label] = _constructor;
+    }
   }
 };
 
@@ -34,12 +63,11 @@ ExternalScripts.try = function(funcName, arguments) {
         script[funcName].call(script, arguments);
       }
       else {
-        console.error("script " + name + " doesn't respond to  " + funcName);
+        this.externalScriptError("script " + name + " doesn't respond to  " + funcName);
       }
     }
     catch (exception) {
-      console.error("Error calling " + funcName + " on "+ name);
-      console.error(exception);
+      this.externalScriptError(exception, {name: name, funcName: funcName});
     }
   }
 }
