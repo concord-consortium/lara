094dfc370f07ab8ab8edfef4b0e88a56c703d7af
pjanik <janikpiotrek@gmail.com>
2018-05-31 15:27:43 +0200

    Add migration that converts interactive items into page items
    [#157605358]
    
    And provide an easy way to revert that change

---
+++ db/migrate/20180530151905_convert_interactive_items_to_page_items.rb
@@ -0,0 +1,9 @@
+class ConvertInteractiveItemsToPageItems < ActiveRecord::Migration
+  def up
+    execute "INSERT INTO page_items (interactive_page_id, embeddable_id, embeddable_type, position, created_at, updated_at, section) SELECT interactive_page_id, interactive_id, interactive_type, position, created_at, updated_at, 'interactive_box' FROM interactive_items"
+  end
+
+  def down
+    execute "DELETE FROM page_items WHERE section='interactive_box'"
+  end
+end
