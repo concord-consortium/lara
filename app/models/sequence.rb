c865c69d27f22d032c229ea000cc6d3448166ebc
pjanik <janikpiotrek@gmail.com>
2018-07-09 15:05:51 +0200

    Mark Sequence Abstract depreciated, add relevant comments
    [#158870718]

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -123,6 +123,9 @@ class Sequence < ActiveRecord::Base
       # Otherwise, the old Portal code would reset its description copy each time the sequence was published.
       # When all Portals are upgraded to v1.31 we can stop sending this property.
       'description' => self.description,
+      # Abstract is not used by new Portal anymore. However, we still need to send it to support older Portal instances.
+      # Otherwise, the old Portal code would reset its abstract copy each time the sequence was published.
+      # When all Portals are upgraded to v1.31 we can stop sending this property.
       'abstract' => self.abstract,
       "url" => local_url,
       "create_url" => local_url,
