4a5a6b811c596aa486ec38c1704f6131101aed8c
pjanik <janikpiotrek@gmail.com>
2018-09-06 18:28:37 +0200

    Include text-decorator using assets pipeline
    [#158302431]

--- app/assets/javascripts/runtime.js
+++ app/assets/javascripts/runtime.js
@@ -28,6 +28,7 @@
 //= require jquery.jcarousel
 //= require responsive-layout
 //= require shutterbug
+//= require text-decorator-1.0.0.umd.js
 //= require page-unload-warning
 //= require modals
 //= require modal-dialog
