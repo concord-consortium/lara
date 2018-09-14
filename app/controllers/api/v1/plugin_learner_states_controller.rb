acd7437579a3665fce7caba36d726149e0390228
Noah Paessel <knowuh@gmail.com>
2018-09-14 13:07:46 -0400

    Add authorization checks for PluginLearnerStates & add tests.

--- app/controllers/api/v1/plugin_learner_states_controller.rb
+++ app/controllers/api/v1/plugin_learner_states_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::PluginLearnerStatesController < ApplicationController
   STATUS_OK = 200
   STATUS_ERROR = 500
   STATUS_NOT_FOUND = 404
+  STATUS_NOT_AUTHORIZED = 403
 
   private
   def fail(status, message, err)
@@ -9,6 +10,14 @@ class Api::V1::PluginLearnerStatesController < ApplicationController
         response_type: "ERROR",
         error: "#{message} #{err}"
       }, status: status
+  end
+
+  def notAuthorized(err)
+    fail(
+      STATUS_NOT_AUTHORIZED,
+      "Not authorized: for user: #{current_user && current_user.id}",
+      err
+    )
   end
 
   def success(state_data)
@@ -19,28 +28,42 @@ class Api::V1::PluginLearnerStatesController < ApplicationController
     run_id = params[:run_id]
     plugin_id = params[:plugin_id]
     begin
-      run = Run.find(run_id)
+      @run = Run.find(run_id)
       plugin  = Plugin.find(plugin_id)
-      @p_state = PluginLearnerState.find_or_create(plugin, run)
+      @p_state = PluginLearnerState.find_or_create(plugin, @run)
+    rescue CanCan::AccessDenied => err
+      notAuthorized(err)
     rescue => err
-      fail(STATUS_NOT_FOUND, "No state for plugin_id: #{plugin_id}, run_id: #{run_id}", err)
+      fail(
+        STATUS_NOT_FOUND,
+        "No state for plugin_id: #{plugin_id}, run_id: #{run_id}",
+        err
+      )
     end
   end
 
   public
   def load
     getPluginState
+    authorize! :access, @run
     success(@p_state.state)
   end
 
   def save
     getPluginState
     begin
+      authorize! :access, @run
       state = params[:state]
       @p_state.update_attribute(:state, state)
       success(@p_state.reload.state)
+    rescue CanCan::AccessDenied => err
+      notAuthorized(err)
     rescue => err
-      fail(STATUS_ERROR, "Couldn't update state for PluginLearnerState(#{@p_state.id})", err)
+      fail(
+        STATUS_ERROR,
+        "Couldn't update state for PluginLearnerState(#{@p_state.id})",
+        err
+      )
     end
   end
 
--- spec/controllers/api/v1/plugin_learner_states_controller_spec.rb
+++ spec/controllers/api/v1/plugin_learner_states_controller_spec.rb
@@ -9,13 +9,28 @@ describe Api::V1::PluginLearnerStatesController do
   let(:run_id)        { 442 }
 
   let(:user_opts) do
-    { id: 42}
+    {
+      id: 42,
+      email: "student@school.edu",
+      admin?: false,
+      author?: false
+    }
+  end
+
+  let(:user2_opts) do
+    {
+      id: 44,
+      email: "student@school.edu",
+      admin?: false,
+      author?: false
+    }
   end
 
   let(:run_opts) do
     {
       id: run_id,
-      user: user
+      user: user,
+      user_id: user.id
     }
   end
 
@@ -26,9 +41,10 @@ describe Api::V1::PluginLearnerStatesController do
     }
   end
 
-  let(:user)   { double("User",   user_opts) }
-  let(:run)    { double("Run",    run_opts) }
-  let(:plugin) { double("Plugin", plugin_opts) }
+  let(:user)   { mock_model User, user_opts }
+  let(:user2)  { mock_model User, user2_opts }
+  let(:run)    { mock_model Run, run_opts }
+  let(:plugin) { mock_model Plugin, plugin_opts }
   let(:state)  { 'state_value' }
 
   before(:each) do
@@ -39,19 +55,86 @@ describe Api::V1::PluginLearnerStatesController do
     pstate.update_attribute(:state, state)
   end
 
-  describe 'load' do
-    it "should load data" do
-      post :load, run_id: run_id, plugin_id: plugin_id
-      expect(response.status).to eq(200)
-      expect(json_response_body['state']).to eq("state_value")
+  describe "When a user of a run uses a plugin" do
+    before(:each) do
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+    describe 'to load state' do
+      it "they should get their state" do
+        post :load, run_id: run_id, plugin_id: plugin_id
+        expect(response.status).to eq(200)
+        expect(json_response_body['state']).to eq(state)
+      end
+    end
+
+    describe 'to save state' do
+      it "they should save their state" do
+        post :save, run_id: run_id, plugin_id: plugin_id, state: "new_state"
+        expect(response.status).to eq(200)
+        expect(json_response_body['state']).to eq("new_state")
+      end
     end
   end
-  
-  describe 'save' do
-    it "should save data, and return the same data" do
-      post :save, run_id: run_id, plugin_id: plugin_id, state: "new_state"
-      expect(response.status).to eq(200)
-      expect(json_response_body['state']).to eq("new_state")
+
+  describe "When a user tries to use a plugin on someone elses run" do
+    before(:each) do
+      allow(controller).to receive(:current_user).and_return(user2)
+    end
+    describe 'to load state' do
+      it "they should not get the state from the other user" do
+        post :load, run_id: run_id, plugin_id: plugin_id
+        expect(response.status).to eq(403)
+      end
+    end
+
+    describe 'to save state' do
+      it "they should not be able to save over the other users state" do
+        post :save, run_id: run_id, plugin_id: plugin_id, state: "new_state"
+        expect(response.status).to eq(403)
+      end
+    end
+  end
+
+  describe "for an anonymous user using a pliugin for someone elses run" do
+    before(:each) do
+      allow(controller).to receive(:current_user).and_return(nil)
+    end
+
+    describe 'to load state' do
+      it "they should be prevented from seeing someone elses state" do
+        post :load, run_id: run_id, plugin_id: plugin_id
+        expect(response.status).to eq(403)
+      end
+    end
+
+    describe 'to save state' do
+      it "should not allow anonymous user to save over other users state" do
+        post :save, run_id: run_id, plugin_id: plugin_id, state: "new_state"
+        expect(response.status).to eq(403)
+      end
+    end
+
+    describe "for an anonymous user using a plugin in their own run" do
+      let(:run_opts) do
+        {
+          id: run_id,
+          user: nil,
+          user_id: nil
+        }
+      end
+      describe 'to load state' do
+        it "they should be able to load their state" do
+          post :load, run_id: run_id, plugin_id: plugin_id
+          expect(response.status).to eq(200)
+        end
+      end
+
+      describe 'to save state' do
+        it "they should be able to save their state" do
+          post :save, run_id: run_id, plugin_id: plugin_id, state: "new_state"
+          expect(response.status).to eq(200)
+        end
+      end
     end
   end
 end
