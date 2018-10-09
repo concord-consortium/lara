12e40609bfd21e3099d5e2da27df9103f15ff9e6
Noah Paessel <knowuh@gmail.com>
2018-10-09 16:56:43 -0400

    new scaling logic in interactiveSizing.js:
    Limit interactive height to viewable screen size.  Iframe fits inside the viewport
    
    [#160601386]
    https://www.pivotaltracker.com/story/show/160601386

--- app/assets/javascripts/interactive-sizing.js
+++ app/assets/javascripts/interactive-sizing.js
@@ -5,17 +5,22 @@ function interactiveSizing () {
     var $iframe = $(this);
     var aspectRatio = $iframe.data('aspect-ratio');
     var resizeMethod = $iframe.data('aspect-ratio-method');
-    var currWidth = $iframe.width();
+    var magicNumber = 125;
+    var maxHeight = window.innerHeight - magicNumber;
 
     if(resizeMethod === 'MAX') {
-      magicNumber = 125;
-      newHeight = window.innerHeight - magicNumber;
-      $iframe.attr('height', newHeight);
+      $iframe.height(maxHeight);
+      $iframe.attr('width', '100%');
     }
     else {
-      $iframe.attr('height', currWidth / aspectRatio);
+      $iframe.attr('width', '100%');
+      $iframe.height($iframe.width() / aspectRatio);
+      if ($iframe.height() > maxHeight) {
+        var scale = maxHeight / $iframe.height();
+        $iframe.attr('width', scale * 100 + '%');
+        $iframe.height(maxHeight);
+      }
     }
-    $iframe.attr('width', '100%');
   }
 
   $('[data-aspect-ratio]').each(function () {
@@ -23,6 +28,7 @@ function interactiveSizing () {
     $iframe.on('sizeUpdate', setSize);
     $iframe.trigger('sizeUpdate');
   });
+
 }
 
 $(document).ready(interactiveSizing);
