7471669c8eeb1ebc57c044679fe5a25d232cdbf4
Noah Paessel <knowuh@gmail.com>
2018-10-09 14:57:51 -0400

    Place window resizing logic in 'interactive-sizing.js'

--- app/assets/javascripts/interactive-sizing.js
+++ app/assets/javascripts/interactive-sizing.js
@@ -4,9 +4,18 @@ function interactiveSizing () {
   function setSize () {
     var $iframe = $(this);
     var aspectRatio = $iframe.data('aspect-ratio');
-    var currWidth = $iframe.width()
+    var resizeMethod = $iframe.data('aspect-ratio-method');
+    var currWidth = $iframe.width();
+
+    if(resizeMethod === 'MAX') {
+      magicNumber = 125;
+      newHeight = window.innerHeight - magicNumber;
+      $iframe.attr('height', newHeight);
+    }
+    else {
+      $iframe.attr('height', currWidth / aspectRatio);
+    }
     $iframe.attr('width', '100%');
-    $iframe.attr('height', currWidth / aspectRatio);
   }
 
   $('[data-aspect-ratio]').each(function () {
