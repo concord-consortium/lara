bb6d0dca8a6a1fd4b754c3ec3a5a6ae22b6fa69e
Noah Paessel <knowuh@gmail.com>
2018-10-30 16:08:53 -0400

    [DL/NP]: Refactor `plugin_helper.rb`.

--- app/helpers/plugin_helper.rb
+++ app/helpers/plugin_helper.rb
@@ -1,13 +1,20 @@
 module PluginHelper
 
+  # def plugin_loaded(url)
+  #   @loaded_plugins ||= {}
+  #   if @loaded_plugins[url]
+  #     true
+  #   else
+  #     @loaded_plugins[url] = true
+  #     false
+  #   end
+  # end
+
   def plugin_loaded(url)
     @loaded_plugins ||= {}
-    if @loaded_plugins[url]
-      true
-    else
-      @loaded_plugins[url] = true
-      false
-    end
+    return true if @loaded_plugins[url]
+    @loaded_plugins[url] = true
+    false
   end
 
 end
