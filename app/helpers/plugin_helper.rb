56a199ae6d29897a0aa5d442534f2af99f5d2ecc
Noah Paessel <knowuh@gmail.com>
2018-10-30 20:34:19 -0400

    [DL/NP]: Add Icon to Teacher Edition Banner.

--- app/helpers/plugin_helper.rb
+++ app/helpers/plugin_helper.rb
@@ -1,15 +1,5 @@
 module PluginHelper
 
-  # def plugin_loaded(url)
-  #   @loaded_plugins ||= {}
-  #   if @loaded_plugins[url]
-  #     true
-  #   else
-  #     @loaded_plugins[url] = true
-  #     false
-  #   end
-  # end
-
   def plugin_loaded(url)
     @loaded_plugins ||= {}
     return true if @loaded_plugins[url]
