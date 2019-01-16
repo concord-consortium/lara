4b721f2661636a7ce12b9f17060993adb052ebb5
Doug Martin <dmartin@concord.org>
2019-01-16 12:23:55 -0500

    Add interactive question names to header if they exist [#163060115]

--- cypress/cypress/integration/runtime/interactive-questions.js
+++ cypress/cypress/integration/runtime/interactive-questions.js
@@ -14,8 +14,13 @@ context('Interactive embeddables', function () {
       cy.visitActivityPage(activityUrl, 0)
 
       // the second embeddable is an interactive that saves state so it should have a header
+      // but it does not have a name so the header should only have the question number
       cy.get(".embeddables div:nth-child(2) .interactive-container").should("exist")
-      cy.get(".embeddables div:nth-child(2) .question-hdr").should("contain", "Question #2")
+      cy.get(".embeddables div:nth-child(2) .question-hdr").should(($p) => {
+        expect($p.length).to.equal(1)
+        const text = $p[0].innerText.trim();
+        expect(text).to.equal("Question #2");
+      })
 
       // the fourth embeddable is an interactive that does not save state so it should not have a header
       cy.get(".embeddables div:nth-child(4) .interactive-container").should("exist")
@@ -23,7 +28,7 @@ context('Interactive embeddables', function () {
 
       // the first interactive saves state so it should have a header
       cy.get(".interactive-mod div:nth-child(1) .interactive-container").should("exist")
-      cy.get(".interactive-mod div:nth-child(1) .question-hdr").should("contain", "Question #4")
+      cy.get(".interactive-mod div:nth-child(1) .question-hdr").should("contain", "Question #4: Saves state #1")
       
       // the second interactive does not save state so it should not have a header
       cy.get(".interactive-mod div:nth-child(2) .interactive-container").should("exist")
@@ -31,7 +36,7 @@ context('Interactive embeddables', function () {
       
       // the third interactive saves state so it should have a header
       cy.get(".interactive-mod div:nth-child(3) .interactive-container").should("exist")
-      cy.get(".interactive-mod div:nth-child(3) .question-hdr").should("contain", "Question #5")
+      cy.get(".interactive-mod div:nth-child(3) .question-hdr").should("contain", "Question #5: saves state #2")
     })
   })
   
