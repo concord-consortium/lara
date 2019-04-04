688859b11a282b843e27df499a969014a5f42399
Unknown <emcelroy@concord.org>
2019-04-03 20:36:04 -0400

    Add GeoHazard to list of themes to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,8 @@ LightweightStandalone::Application.configure do
   theme-has-ngs-water.css theme-rites.css print-summary.css theme-interactions-electrostatics.css
   theme-interactions.css theme-inquiryspace.css theme-itsi.css theme-itsi-survey.css theme-has-astro.css
   theme-has-astro-dark.css theme-ngss-assessment.css theme-building-models.css theme-geode-himalayas.css
-  theme-ipums-terra.css theme-data-games.css theme-precipitating-change.css theme-geniventure.css theme-connectedbio.css)
+  theme-ipums-terra.css theme-data-games.css theme-precipitating-change.css theme-geniventure.css
+  theme-connectedbio.css theme-geohazard.css)
   # CSS file used by TinyMCE iframe:
   config.assets.precompile += %w(tinymce-content.css)
   # Disable delivery errors, bad email addresses will be ignored
