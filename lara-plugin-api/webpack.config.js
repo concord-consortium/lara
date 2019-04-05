1a58e923f15c88910bfd2a89ad9f9b3c1508e52c
pjanik <janikpiotrek@gmail.com>
2019-04-05 14:33:48 +0200

    Add lara-plugin-api npm package based on TypeScript
    [#165062314]

---
+++ lara-plugin-api/webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = (env, argv) => {
+  return {
+    context: __dirname, // to automatically find tsconfig.json
+    devtool: 'source-map',
+    entry: './src/index.tsx',
+    mode: 'development',
+    output: {
+      filename: 'lara-plugin-api-ts.js',
+      library: 'LARAPluginAPI',
+      libraryTarget: 'umd'
+    },
+    performance: { hints: false },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          enforce: 'pre',
+          use: [
+            {
+              loader: 'tslint-loader',
+              options: {}
+            }
+          ]
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          options: {
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: [ '.ts', '.tsx', '.js' ]
+    },
+    stats: {
+      // suppress "export not found" warnings about re-exported types
+      warningsFilter: /export .* was not found in/
+    },
+    plugins: [
+      new ForkTsCheckerWebpackPlugin()
+    ]
+  };
+};
