d1e0dced7906bfc31ddb43255ac386b8898adace
Scott Cytacki <scytacki@concord.org>
2019-04-23 16:49:41 -0400

    add more specs to check the append_white_list_params

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -43,8 +43,25 @@ describe ApplicationHelper do
     describe "with params" do
       let(:params) { {"mode" => "teacher-edition", "foo" => "bar", "baz" => "boom"} }
 
+      describe "with query param without a value" do
+        let(:url) { "/foo?bar" }
+        # This is documenting broken behavior, however this case should not cause problems
+        # in practice
+        it "adds a '=' to the url and the whitelisted params" do
+          expect(append_white_list_params(url)).to eql("/foo?bar=&mode=teacher-edition")
+        end
+      end
+
       it "should return the passed url plus append the current default whitelist params" do
         expect(append_white_list_params(url)).to eql("/foo?bar=1&mode=teacher-edition")
+      end
+
+      describe "with a url that already contain a whitelisted param" do
+        let(:url) { "/foo?bar=1&mode=teacher-edition" }
+
+        it "should not duplicate query params that match whitelist parameters" do
+          expect(append_white_list_params(url)).to eql("/foo?bar=1&mode=teacher-edition")
+        end
       end
 
       it "should return the passed url plus append the current passed whitelist params" do
@@ -57,6 +74,18 @@ describe ApplicationHelper do
 
       it "should return the passed url when the whitelist params does not intersect" do
         expect(append_white_list_params(url, ["bing"])).to eql(url)
+      end
+
+      describe "with query string that includes escaped characters" do
+        let(:domain) { "https://example.com" }
+        let(:query_param_1) { "server=https%3A%2F%2Fexample.com%2Flatest%3Fmode%3Dsomething%26test" }
+        let(:query_param_2) { "scaling=true" }
+        let(:url) { "#{domain}?#{query_param_1}&#{query_param_2}" }
+        # the code seems to order the parameters alphabetically
+        let(:result) { "#{domain}?mode=teacher-edition&#{query_param_2}&#{query_param_1}" }
+        it "should return the url plus the whitelisted params" do
+          expect(append_white_list_params(url)).to eql(result)
+        end
       end
     end
   end
