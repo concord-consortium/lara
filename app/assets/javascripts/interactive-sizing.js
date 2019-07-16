f54062431dbc3e75ce94ff4c5959f2785fb1ba9b
pjanik <janikpiotrek@gmail.com>
2019-07-16 14:52:39 +0200

    Refactor interactive sizing module to fix a few issues
    [#167044217]
    
    - start the whole process on document 'load', not 'ready', so the images
      are loaded when we start the calculations
    - fix too short header which was not matching interactive sometimes
    - explicitly handle multiple interactives in the pinned container
    - add more comments

--- app/assets/javascripts/interactive-sizing.js
+++ app/assets/javascripts/interactive-sizing.js
@@ -1,6 +1,22 @@
 /*global $ */
 
 function interactiveSizing () {
+  function setInteractiveWidth($iframe, width) {
+    $iframe.css('width', width);
+    var $header = $iframe.parents(".embeddable-container").find(".question-hdr")
+    if ($header.length > 0) {
+      $header.css('width', width);
+    }
+  }
+
+  function limitInteractiveHeight($iframe, maxHeight) {
+    if ($iframe.height() > maxHeight) {
+      var scale = (maxHeight / $iframe.height()) * 100 + '%';
+      setInteractiveWidth($iframe, scale);
+      $iframe.css('height', maxHeight);
+    }
+  }
+
   function setSize () {
     var $iframe = $(this);
     var aspectRatio = $iframe.data('aspect-ratio');
@@ -8,35 +24,43 @@ function interactiveSizing () {
     var maxHeight = window.innerHeight;
 
     var $pinned = $iframe.parents('.pinned');
-    if ($pinned.length > 0) {
-      // If interactive is pinned, make sure that maxHeight calculations don't cause interactive to become unpinned
-      // (as it gets too tall). Calculate the current difference between height of the interactive and its container.
-      // That will automatically handle question header (when interactive saves state) and all the possible margins
-      // and padding. Also, take into account a pinned interactive offset (space between the window top border and
-      // the interactive container top edge).
+    var isPinned = $pinned.length > 0;
+    var multipleInteractives = isPinned && $pinned.children().length > 1;
+
+    // Reset dimensions.
+    setInteractiveWidth($iframe, '100%');
+
+    if (isPinned && multipleInteractives) {
+      // This case is treated in a basic way for now. MAX setting doesn't make much sense.
+      // We just set aspect ratio (either provided by user or interactive) and we don't limit interactive height.
+      // So, interactives might get 'unpinned' as a result. It could have been treated better in the future,
+      // if there's a need for that.
+      $iframe.css('height', $iframe.width() / aspectRatio);
+      return;
+    }
+
+    if (isPinned) {
+      // If interactive is pinned (and there's only one interactive), make sure that maxHeight calculations don't
+      // cause interactive to become unpinned (as it gets too tall). Calculate the current difference between height
+      // of the interactive and its container. That will automatically handle question header (when interactive saves
+      // state) and all the possible margins and padding. Also, take into account a pinned interactive offset (space
+      // between the window top border and the interactive container top edge).
       var headersHeight = $pinned.height() - $iframe.height();
       maxHeight -= headersHeight + window.SCROLL_INTERACTIVE_MOD_OFFSET;
     }
-    $iframe.attr('width', '100%');
 
     if (resizeMethod === 'MAX') {
-      $iframe.height(maxHeight);
+      $iframe.css('height', maxHeight);
     }
     else if (resizeMethod === 'MANUAL') {
-      $iframe.height($iframe.width() / aspectRatio);
+      $iframe.css('height', $iframe.width() / aspectRatio);
     }
     else /* (resizeMethod === 'DEFAULT') */ {
-      $iframe.height($iframe.width() / aspectRatio);
-      if ($iframe.height() > maxHeight) {
-        var scale = (maxHeight / $iframe.height()) * 100 + '%';
-        $iframe.attr('width', scale);
-        $iframe.height(maxHeight);
-        // Rescale question header too (it's present when interactive saves state).
-        var $header = $iframe.parents(".embeddable-container").find(".question-hdr")
-        if ($header.length > 0) {
-          $header.css('width', scale);
-        }
-      }
+      $iframe.css('height', $iframe.width() / aspectRatio);
+      // PJ [7/16/2019]: Why is the height limited only in the DEFAULT mode, but not in MANUAL?
+      // I don't find this intuitive, but it has been implemented because of some issues described
+      // in this story: https://www.pivotaltracker.com/n/projects/736901/stories/161458506
+      limitInteractiveHeight($iframe, maxHeight);
     }
   }
 
@@ -45,8 +69,9 @@ function interactiveSizing () {
     $iframe.on('sizeUpdate', setSize);
     $iframe.trigger('sizeUpdate');
   });
-
 }
 
-$(document).ready(interactiveSizing);
-
+// Use 'load' event so all the images are loaded before we start calculating size.
+// Note that wrapping plugins can add images and other elements around interactive.
+// 'load' event should handle that (in contrast to 'ready').
+$(window).on('load', interactiveSizing);
