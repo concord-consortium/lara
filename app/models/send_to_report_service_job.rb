afd2ef5a1c97dec147618ea7cb083ca4691a80e2
Noah Paessel <knowuh@gmail.com>
2019-07-17 13:23:51 -0400

    Attempt to reduce cognitive complexity of the perform function.
    Code climate gave me a 9 on cognitive complexity for `perform` here.
    
    Not sure if its the pre-emptive returns, or the `unless` syntax.  The docs aren't super clear
    on what can be done in situations like this.  I started to break the method up, but it started
    to feel more complex to me.
    
    https://docs.codeclimate.com/docs/cognitive-complexity
    
    [#165204332]
    
    https://www.pivotaltracker.com/story/show/165204332

--- app/models/send_to_report_service_job.rb
+++ app/models/send_to_report_service_job.rb
@@ -26,24 +26,24 @@ class SendToReportServiceJob < Struct.new(:publishable_type, :publishable_id, :q
 
     # Skip if the publishable has changed since enqueuing. Removes from queue.
     # A new job will be scheduled. The new job should be run instead of this one.
-    return if publishable.updated_at > queuetime
+    return if (publishable.updated_at > queuetime)
     resource_sender = ReportService::ResourceSender.new(publishable)
 
     # Skip it if nothing interesting changed. Removes from queue.
-    return if resource_sender.payload_hash == publishable.last_report_service_hash
+    return if (resource_sender.payload_hash == publishable.last_report_service_hash)
 
     result = resource_sender.send()
-    unless (result && result["success"])
+    
+    if result["success"]
+      # Record the last payload_hash to the publishable.
+      publishable.update_column(:last_report_service_hash, resource_sender.payload_hash)
+      # Success: If we made it to the end of our fuction this job will
+      # be removed from delayed job queue.
+    else
       # Delayed job automatically retries failed `perform`s.
       # rescheduled in 5 seconds + N ** 4
       raise FailedToSendToReportService.new("Failed to send")
     end
-
-    # Record the last payload_hash to the publishable.
-    publishable.update_column(:last_report_service_hash, resource_sender.payload_hash)
-
-    # Success: If we made it to the end of our fuction this job will
-    # be removed from delayed job queue.
   end
 
   def max_attempts
