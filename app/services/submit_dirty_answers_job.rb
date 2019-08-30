84351e9c380848f38fb166a9fc7a8176a87c5834
pjanik <janikpiotrek@gmail.com>
2019-08-30 17:50:48 +0200

    Add logging and fix job error handling while sending run to report service
    [#168170575]

--- app/services/submit_dirty_answers_job.rb
+++ app/services/submit_dirty_answers_job.rb
@@ -4,14 +4,15 @@ class SubmitDirtyAnswersJob < Struct.new(:run_id, :start_time)
   end
 
   def send_to_report_service(run)
-    begin
-      # only send answers which are 'dirty':
-      sender = ReportService::RunSender.new(run, {send_all_answers: false})
-      sender.send()
-    rescue => e
-      Rails.logger.error("Couldn't send run #{run.key} to report service:")
-      Rails.logger.error(e)
-    end
+    # only send answers which are 'dirty':
+    sender = ReportService::RunSender.new(run, {send_all_answers: false})
+    response = sender.send
+    Rails.logger.info(
+      "Run sent to report service, success: #{response.success?}, " +
+      "num_answers: #{run.answers.count}, #{run.run_info_string}, " +
+      "response: #{response.code} #{response.message} #{response.body}"
+    )
+    run.abort_job_and_requeue(run.run_info_string) unless response.success?
   end
 
   def perform
@@ -19,20 +20,18 @@ class SubmitDirtyAnswersJob < Struct.new(:run_id, :start_time)
     # Find array of dirty answers and send them to the portal
     da = run.dirty_answers
     return if da.empty?
-    if run.send_to_portal(da, start_time)
-      # if there is a report service configured send run data there
-      send_to_report_service(run) if ReportService::configured?
-      run.set_answers_clean(da) # We're only cleaning the same set we sent to the portal
-      run.reload
-      if run.dirty_answers.empty?
-        run.mark_clean
-      else
-        # I'm not sure about using this method here, because it raises an error when the
-        # "problem" may just be that (a) new dirty answer(s) were submitted between the
-        # send_to_portal call and the check here.
-        run.abort_job_and_requeue
-      end
+    run.send_to_portal(da, start_time)
+    # if there is a report service configured send run data there
+    send_to_report_service(run) if ReportService::configured?
+    # We're only cleaning the same set we sent to the portal and report service
+    run.set_answers_clean(da)
+    run.reload
+    if run.dirty_answers.empty?
+      run.mark_clean
     else
+      # I'm not sure about using this method here, because it raises an error when the
+      # "problem" may just be that (a) new dirty answer(s) were submitted between the
+      # send_to_portal call and the check here.
       run.abort_job_and_requeue
     end
   end
