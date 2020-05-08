7a835ebdbe6b9f5e0e51dae4a410efdcd723ba43
Doug Martin <dmartin@concord.org>
2020-05-08 16:04:13 -0400

    Moved picked to popup editor

--- lara-typescript/src/page-item-admin/index.tsx
+++ lara-typescript/src/page-item-admin/index.tsx
@@ -1,9 +1,25 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { ManagedInteractiveAdmin } from "./managed-interactives-admin";
+import { ManagedInteractiveAdmin, ILibraryInteractive } from "./managed-interactives-admin";
 
-const renderManagedInteractiveAdmin = (root: HTMLElement) => ReactDOM.render(<ManagedInteractiveAdmin />, root);
+// by default it is set to render within the managed interactive popup but this can be changed by
+// specifing a different id and name that matches the form that it is enclosed within
+interface IRenderManagedInteractiveProps {
+  selectLegend?: string;
+  selectId?: string;
+  selectName?: string;
+  libraryInteractive?: ILibraryInteractive;
+}
+const renderManagedInteractiveAdmin = (root: HTMLElement, props?: IRenderManagedInteractiveProps) => {
+  return ReactDOM.render(
+    <ManagedInteractiveAdmin
+      selectLegend={props ? props.selectLegend : undefined}
+      selectId={props ? props.selectId : undefined}
+      selectName={props ? props.selectName : undefined}
+      libraryInteractive={props ? props.libraryInteractive : undefined}
+    />, root);
+};
 
 export {
   ManagedInteractiveAdmin,
