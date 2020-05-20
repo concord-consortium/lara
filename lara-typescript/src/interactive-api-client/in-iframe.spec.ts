11ef514dddf05e9e3a56322b13553e57157c22a6
Doug Martin <dmartin@concord.org>
2020-05-20 08:07:13 -0400

    Added in-iframe-spec

---
+++ lara-typescript/src/interactive-api-client/in-iframe.spec.ts
@@ -0,0 +1,8 @@
+import { InIframe } from "./in-frame";
+
+describe("in-iframe", () => {
+  it("should export InIframe", () => {
+    expect(InIframe).toBeDefined();
+    expect(InIframe).toBe(false);
+  });
+});
