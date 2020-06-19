20e0514accdb97a4c45765d7b88234e0c6dd6281
Kevin Santos <ksantos@CC-KSANTOS.local>
2020-06-19 17:56:50 -0400

    setup tests to run with fixture data

--- cypress/cypress/integration/runtime/header-footer.js
+++ cypress/cypress/integration/runtime/header-footer.js
@@ -1,146 +1,122 @@
-context('Header and Footer testing', function () {
+context("Header and Footer testing", function () {
+  // Need to get all themes synced in order to run on all environments.
+  // Tests only run on 2 themese currently
+  let activityUrl;
+  const activityPath = "activities/activity_with_arg_block_section.json";
+  const themes = [
+    "Default",
+    "HAS National Geographic",
+    // "WATERS",
+    // "Precipitating Change",
+    // "InquirySpace"
+  ];
 
-    let baseUrl = 'https://authoring.staging.concord.org'
-    let activity = '/activities/20634'
-    let pages = ['307396']
-    let themes = ['Default','WATERS', 'Precipitating Change', 'InquirySpace', 'HAS National Geographic']
-    
-    let pageID = (index) => {
-        let pageUrl = '/pages/' + pages[index]
-        return pageUrl
-    }
+  beforeEach(() => {
+    cy.login();
+    cy.importMaterial(activityPath).then((url) => (activityUrl = url));
+  });
 
-    beforeEach(() => {
-        cy.visit(baseUrl+activity)
-        cy.login()
-    })
+  afterEach(() => {
+    cy.deleteMaterial(activityUrl);
+  });
 
-    function editActivityTheme(i) {
-        cy.visit(baseUrl + activity + '/edit')
-        cy.get('select#lightweight_activity_theme_id').select(themes[i])
-        saveActivity()
-    }
+  function editActivityTheme(i) {
+    cy.visit(activityUrl + "/edit");
+    cy.get("select#lightweight_activity_theme_id").parent().click({ force: true }).then(() => {
+        cy.get("select#lightweight_activity_theme_id").select(themes[i]);
+      });
+    saveActivity();
+  }
+  function viewStudentRuntime() {
+    cy.visit(activityUrl).then(() => {
+      cy.get(".pagination-link").contains("1").click();
+    });
+  }
+  function saveActivity() {
+    cy.get("input#save-top").eq(0).click();
+  }
+  function checkForCCLogo() {
+    cy.get("header").find("img").should("have.attr", "src").should("include", "cc-logo");
+  }
+  function checkForActivityLabel1() {
+    cy.get(".activity-nav-title").should("contain", "[Cypress] Arg Block Test").and("be.visible");
+  }
+  function checkForActivityLabel2() {
+    cy.get(".content-hdr").last().should("contain", "[Cypress] Arg Block Test").and("exist");
+  }
+  function checkForMenuButton() {
+    cy.get("a#menu-trigger").should("be.visible").and("contain", "Menu").find("i.fa-bars").should("be.visible");
+  }
+  function checkForPrintLabelButton() {
+    cy.get("li.am-print").find(".text").should("contain", "Print");
+  }
+  function checkForPrintSideButton() {
+    cy.get("li.am-report").find("i.fa-print").should("exist").and("be.visible");
+  }
+  function checkForFooterPageNavByNumber() {
+    cy.get(".footer-nav").find(".pages").should("be.visible").and("contain", "1");
+  }
+  function checkForFooterPageNavByNextPrev() {
+    cy.get(".footer-nav").find(".prev_and_next").within(() => {
+        cy.get(".prev.disabled").should("exist");
+        cy.get(".next").should("have.attr", "href").should("include", "/activities");
+      });
+  }
+  function checkForFooterPageNavByNextPrevAlt() {
+    cy.get(".bottom-buttons").within(() => {
+      cy.get(".forward_nav").find("input.next").should("contain", "Next").and("be.visible");
+    });
+  }
+  /**
+   * ActivityLabel1 is where the Activity Name is displayed in the same div element as the top level page numbers
+   * ActivityLabel2 is where the Activity Name is displayed in larger text above the introduction LARA component
+   */
 
-    function saveActivity() {
-        cy.get('input#save-top').eq(0).click()
-    }
+  describe("Testing Header and Footers in LARA runtime", () => {
+    it("Test Default, Data Games, NextGen themes", () => {
+      editActivityTheme(0);
+      viewStudentRuntime();
 
-    function checkForCCLogo() {
-        cy.get('header').find('img').should('have.attr', 'src').should('include','cc-logo')
-    }
+      checkForCCLogo();
+      checkForActivityLabel2();
+      checkForPrintSideButton();
+    });
+    it.skip("Test WATERS themes", () => {
+      editActivityTheme(1);
+      viewStudentRuntime();
 
-    function checkForActivityLabel1() {
-        cy.get('.activity-nav-title').should('contain', 'Activity: Library Interactive Test').and('be.visible')
-    }
+      checkForActivityLabel1();
+      checkForActivityLabel2();
+      checkForPrintLabelButton();
+      checkForFooterPageNavByNumber();
+      checkForFooterPageNavByNextPrev();
+    });
+    it.skip("Test Precipitating Change themes", () => {
+      editActivityTheme(2);
+      viewStudentRuntime();
 
-    function checkForActivityLabel2() {
-        cy.get('.content-hdr').last().should('contain', 'Library Interactive Test').and('be.visible')
-    }
+      checkForActivityLabel1();
+      checkForActivityLabel2();
+      checkForPrintLabelButton();
+    });
+    it.skip("Test Inquiry Space/Building Models/Interactions/RITES Space Theme", () => {
+      editActivityTheme(3);
+      viewStudentRuntime();
 
-    function checkForMenuButton() {
-        cy.get('a#menu-trigger').should('be.visible').and('contain', 'Menu').find('i.fa-bars').should('be.visible')
-    }
+      checkForCCLogo();
+      checkForActivityLabel1();
+      checkForPrintSideButton();
+      checkForMenuButton();
+      checkForFooterPageNavByNextPrevAlt();
+    });
+    it("Test HAS National Geographic Theme", () => {
+      editActivityTheme(1);
+      viewStudentRuntime();
 
-    function checkForPrintLabelButton() {
-        cy.get('li.am-print').find('.text').should('contain', 'Print')
-    }
-
-    function checkForPrintSideButton() {
-        cy.get('li.am-report').find('i.fa-print').should('exist').and('be.visible')
-    }
-
-    function checkForFooterPageNavByNumber() {
-        cy.get('.footer-nav').find('.pages').should('be.visible').and('contain', '1')
-    }
-
-    function checkForFooterPageNavByNextPrev() {
-        cy.get('.footer-nav').find('.prev_and_next').within(() => {
-            cy.get('.prev.disabled').should('exist')
-            cy.get('.next').should('have.attr', 'href').should('include', '/activities')
-        })
-    }
-
-    function checkForFooterPageNavByNextPrevAlt() {
-        cy.get('.bottom-buttons').within(() => {
-            cy.get('.forward_nav').find('input.next').should('contain', 'Next').and('be.visible')
-        })
-    }
-    /**
-     * ActivityLabel1 is where the Activity Name is displayed in the same div element as the top level page numbers
-     * ActivityLabel2 is where the Activity Name is displayed in larger text above the introduction LARA component
-     */
-
-    describe('Testing Header and Footers in LARA runtime', () => {
-        it('Test Default, Data Games, NextGen themes', () => {
-            editActivityTheme(0)
-            saveActivity()
-            cy.visit(baseUrl+activity+pageID(0))
-
-            checkForCCLogo()
-            // checkForActivityLabel1()
-            checkForActivityLabel2()
-            // checkForPrintLabelButton()
-            checkForPrintSideButton()
-            // checkForMenuButton()
-            // checkForFooterPageNavByNumber()
-            // checkForFooterPageNavByNextPrev()
-        })
-        it('Test WATERS themes', () => {
-            editActivityTheme(1)
-            saveActivity()
-            cy.visit(baseUrl+activity+pageID(0))
-
-            // checkForCCLogo()
-            checkForActivityLabel1()
-            checkForActivityLabel2()
-            checkForPrintLabelButton()
-            // checkForPrintSideButton()
-            // checkForMenuButton()
-            checkForFooterPageNavByNumber()
-            checkForFooterPageNavByNextPrev()
-        })
-        it('Test Precipitating Change themes', () => {
-            editActivityTheme(2)
-            saveActivity()
-            cy.visit(baseUrl+activity+pageID(0))
-
-            // checkForCCLogo()
-            checkForActivityLabel1()
-            checkForActivityLabel2()
-            checkForPrintLabelButton()
-            // checkForPrintSideButton()
-            // checkForMenuButton()
-            // checkForFooterPageNavByNumber()
-            // checkForFooterPageNavByNextPrev()
-        })
-        it('Test Inquiry Space/Building Models/Interactions/RITES Space Theme', () => {
-            editActivityTheme(3)
-            saveActivity()
-            cy.visit(baseUrl+activity+pageID(0))
-
-            checkForCCLogo()
-            checkForActivityLabel1()
-            // checkForActivityLabel2()
-            // checkForPrintLabelButton()
-            checkForPrintSideButton()
-            checkForMenuButton()
-            // checkForFooterPageNavByNumber()
-            checkForFooterPageNavByNextPrevAlt()
-        })
-        it('Test HAS National Geographic Theme', () => {
-            editActivityTheme(4)
-            saveActivity()
-            cy.visit(baseUrl+activity+pageID(0))
-
-            checkForCCLogo()
-            checkForActivityLabel1()
-            // checkForActivityLabel2()
-            // checkForPrintLabelButton()
-            // checkForPrintSideButton()
-            checkForMenuButton()
-            //checkForFooterPageNavByNumber()
-            checkForFooterPageNavByNextPrevAlt()
-        })
-
-    })
-})
+      checkForCCLogo();
+      checkForActivityLabel1();
+      checkForMenuButton();
+      checkForFooterPageNavByNextPrevAlt();
+    });
+  });
+});
