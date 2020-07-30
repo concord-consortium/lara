3c328858b03c4207966e1d42f63e72e89a5fdcac
Doug Martin <dmartin@concord.org>
2020-07-30 07:36:19 -0400

    Added debug info

--- script/cypress-on-travis.sh
+++ script/cypress-on-travis.sh
@@ -3,6 +3,7 @@ PIDFILE=$(pwd)/tmp/pids/server.pid
 if [ -f $PIDFILE ]; then
   rm $PIDFILE
 fi
+echo "Starting rails server ..."
 bundle exec rails s -b 0.0.0.0 -d
 
 # setup cypress
@@ -11,11 +12,17 @@ npm i
 bundle exec rake travis:create_user
 cp config/user-config.sample.json config/user-config.json
 
+# DEBUG INFO
+ldd /home/travis/.cache/Cypress/4.8.0/Cypress/Cypress
+
 # make sure the rails sever is running (uses wait-on in package.json)
+echo "Waiting for rails server to start ..."
 npx wait-on http://localhost:3000
+echo "Rails server started!"
 
 # run the tests
 npm run test:travis
 
 # kill the rails server
+echo "Stopping rails server"
 kill -9 $(cat $PIDFILE)
