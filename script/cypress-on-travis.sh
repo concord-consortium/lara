e564e1f77f1f623db5a2569eefe4035122bad410
Doug Martin <dmartin@concord.org>
2020-07-30 06:56:48 -0400

    Added libconf and started rails server

--- script/cypress-on-travis.sh
+++ script/cypress-on-travis.sh
@@ -1,5 +1,19 @@
+# start the rails server
+PIDFILE=$(pwd)/tmp/pids/server.pid
+if [ -f $PIDFILE ]; then
+  rm $PIDFILE
+fi
+bundle exec rails s -b 0.0.0.0 -d
+
+# setup cypress
 cd cypress
 npm i
 bundle exec rake travis:create_user
 cp config/user-config.travis.json config/user-config.json
+
+# run the tests
 npm run test:travis
+
+# kill the rails server
+cd ..
+kill -9 $(cat $PIDFILE)
