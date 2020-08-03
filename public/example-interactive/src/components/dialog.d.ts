7a7741f6cc38242b42b625400403997f3cad70bc
Scott Cytacki <scytacki@concord.org>
2020-08-03 16:57:23 -0400

    don't copy *.d.ts file, use cpx for cross platform support

--- public/example-interactive/src/components/dialog.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { IDialogInitInteractive } from "../../../interactive-api-client";
-interface Props {
-    initMessage: IDialogInitInteractive;
-}
-export declare const DialogComponent: React.FC<Props>;
-export {};
--- public/example-interactive/src/components/report.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { IReportInitInteractive } from "../../../interactive-api-client";
-interface Props {
-    initMessage: IReportInitInteractive;
-}
-export declare const ReportComponent: React.FC<Props>;
-export {};
