c5624ce086729c684ed8aebe6782c8dee5871612
Scott Cytacki <scytacki@concord.org>
2020-08-04 12:05:02 -0400

    update the example interactive in public after merging

--- public/example-interactive/index.js
+++ public/example-interactive/index.js
@@ -31229,6 +31229,8 @@ exports.GetInteractiveListComponent = function (_a) {
                     supportsSnapshotsValue = (_c = supportsSnapshotsRef.current) === null || _c === void 0 ? void 0 : _c.value;
                     supportsSnapshots = supportsSnapshotsValue === "true"
                         ? true : (supportsSnapshotsValue === "false" ? false : undefined);
+                    setError(undefined);
+                    setOutput(undefined);
                     _d.label = 1;
                 case 1:
                     _d.trys.push([1, 3, , 4]);
@@ -31263,6 +31265,106 @@ exports.GetInteractiveListComponent = function (_a) {
 
 /***/ }),
 
+/***/ "./src/example-interactive/src/components/authoring-apis/set-linked-interactives.tsx":
+/*!*******************************************************************************************!*\
+  !*** ./src/example-interactive/src/components/authoring-apis/set-linked-interactives.tsx ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SetLinkedInteractivesComponent = void 0;
+var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+var react_1 = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+var client = __webpack_require__(/*! ../../../../interactive-api-client */ "./src/interactive-api-client/index.ts");
+exports.SetLinkedInteractivesComponent = function (_a) {
+    var setError = _a.setError, setOutput = _a.setOutput;
+    var linkedInteractivesRef = react_1.useRef(null);
+    var linkedStateRef = react_1.useRef(null);
+    var handleCall = function () { return __awaiter(void 0, void 0, void 0, function () {
+        var linkedInteractives, linkedInteractivesValue, linkedState, linkedStateValue;
+        var _a, _b;
+        return __generator(this, function (_c) {
+            setError(undefined);
+            setOutput(undefined);
+            try {
+                linkedInteractivesValue = (((_a = linkedInteractivesRef.current) === null || _a === void 0 ? void 0 : _a.value) || "").trim();
+                linkedInteractives = linkedInteractivesValue.length > 0 ? JSON.parse(linkedInteractivesValue) : undefined;
+            }
+            catch (e) {
+                return [2 /*return*/, setError("Unable to parse linkedInteractives: " + e.toString())];
+            }
+            linkedStateValue = (((_b = linkedStateRef.current) === null || _b === void 0 ? void 0 : _b.value) || "").trim();
+            if (linkedStateValue.length > 0) {
+                linkedState = linkedStateValue;
+            }
+            try {
+                client.setLinkedInteractives({ linkedInteractives: linkedInteractives, linkedState: linkedState });
+                setOutput("*** Request sent ***");
+            }
+            catch (err) {
+                setError(err);
+            }
+            return [2 /*return*/];
+        });
+    }); };
+    var defaultLinkedInteractives = '[\n  {"id": "ID1", "label": "LABEL"},\n  {"id": "ID2", "label": "LABEL"}\n]';
+    return (React.createElement("div", { className: "padded margin monospace" },
+        React.createElement("div", null,
+            React.createElement("small", null, "NOTE: each parameter can be set separately, to leave out a parameter clear the input.")),
+        React.createElement("div", null,
+            "linkedInteractives:",
+            React.createElement("br", null),
+            React.createElement("textarea", { ref: linkedInteractivesRef, defaultValue: defaultLinkedInteractives, rows: 10, cols: 100 })),
+        React.createElement("div", null,
+            "linkedState: ",
+            React.createElement("input", { ref: linkedStateRef })),
+        React.createElement("div", null,
+            React.createElement("button", { onClick: handleCall }, "Send"),
+            " (no response)")));
+};
+
+
+/***/ }),
+
 /***/ "./src/example-interactive/src/components/authoring.tsx":
 /*!**************************************************************!*\
   !*** ./src/example-interactive/src/components/authoring.tsx ***!
@@ -31277,6 +31379,7 @@ exports.AuthoringComponent = void 0;
 var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var react_1 = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var get_interactive_list_1 = __webpack_require__(/*! ./authoring-apis/get-interactive-list */ "./src/example-interactive/src/components/authoring-apis/get-interactive-list.tsx");
+var set_linked_interactives_1 = __webpack_require__(/*! ./authoring-apis/set-linked-interactives */ "./src/example-interactive/src/components/authoring-apis/set-linked-interactives.tsx");
 exports.AuthoringComponent = function (_a) {
     var initMessage = _a.initMessage;
     var _b = react_1.useState("getInteractiveList"), selectedAuthoringApi = _b[0], setSelectedAuthoringApi = _b[1];
@@ -31291,6 +31394,8 @@ exports.AuthoringComponent = function (_a) {
         switch (selectedAuthoringApi) {
             case "getInteractiveList":
                 return React.createElement(get_interactive_list_1.GetInteractiveListComponent, { setError: setAuthoringApiError, setOutput: setAuthoringApiOutput });
+            case "setLinkedInteractives":
+                return React.createElement(set_linked_interactives_1.SetLinkedInteractivesComponent, { setError: setAuthoringApiError, setOutput: setAuthoringApiOutput });
             default:
                 return undefined;
         }
@@ -31300,7 +31405,8 @@ exports.AuthoringComponent = function (_a) {
             React.createElement("legend", null, "Authoring APIs"),
             authoringApiError ? React.createElement("div", { className: "padded margined error" }, authoringApiError.toString()) : undefined,
             React.createElement("select", { value: selectedAuthoringApi, onChange: handleAuthoringApiChange },
-                React.createElement("option", { value: "getInteractiveList" }, "getInteractiveList")),
+                React.createElement("option", { value: "getInteractiveList" }, "getInteractiveList"),
+                React.createElement("option", { value: "setLinkedInteractives" }, "setLinkedInteractives")),
             authoringApiError
                 ? React.createElement("button", { className: "margined-left", onClick: handleClearAuthoringApiError }, "Clear Error")
                 : undefined,
@@ -31427,7 +31533,7 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getInteractiveSnapshot = exports.getLibraryInteractiveList = exports.setLinkedInteractives = exports.getInteractiveList = exports.closeModal = exports.showModal = exports.removeGlobalInteractiveStateListener = exports.addGlobalInteractiveStateListener = exports.removeAuthoredStateListener = exports.addAuthoredStateListener = exports.removeInteractiveStateListener = exports.addInteractiveStateListener = exports.log = exports.getFirebaseJwt = exports.getAuthInfo = exports.setNavigation = exports.setHint = exports.setHeight = exports.setSupportedFeatures = exports.setGlobalInteractiveState = exports.getGlobalInteractiveState = exports.setAuthoredState = exports.getAuthoredState = exports.setInteractiveState = exports.setInteractiveStateTimeout = exports.getInteractiveState = exports.getInitInteractiveMessage = void 0;
+exports.getInteractiveSnapshot = exports.getLibraryInteractiveList = exports.setLinkedInteractives = exports.getInteractiveList = exports.closeModal = exports.showModal = exports.removeGlobalInteractiveStateListener = exports.addGlobalInteractiveStateListener = exports.removeAuthoredStateListener = exports.addAuthoredStateListener = exports.removeInteractiveStateListener = exports.addInteractiveStateListener = exports.log = exports.getFirebaseJwt = exports.getAuthInfo = exports.setNavigation = exports.setHint = exports.setHeight = exports.setSupportedFeatures = exports.setGlobalInteractiveState = exports.getGlobalInteractiveState = exports.setAuthoredState = exports.getAuthoredState = exports.setInteractiveState = exports.setInteractiveStateTimeout = exports.getInteractiveState = exports.getMode = exports.getInitInteractiveMessage = void 0;
 var client_1 = __webpack_require__(/*! ./client */ "./src/interactive-api-client/client.ts");
 var THROW_NOT_IMPLEMENTED_YET = function (method) {
     throw new Error(method + " is not yet implemented in the client!");
@@ -31443,6 +31549,9 @@ exports.getInitInteractiveMessage = function () {
             client.managedState.once("initInteractive", function () { return resolve(client.managedState.initMessage); });
         }
     });
+};
+exports.getMode = function () {
+    return exports.getInitInteractiveMessage().then(function (initInteractiveMessage) { return initInteractiveMessage === null || initInteractiveMessage === void 0 ? void 0 : initInteractiveMessage.mode; });
 };
 exports.getInteractiveState = function () {
     return client_1.getClient().managedState.interactiveState;
@@ -31631,19 +31740,36 @@ exports.closeModal = function (options) {
     client_1.getClient().post("closeModal", options);
 };
 exports.getInteractiveList = function (options) {
-    return new Promise(function (resolve) {
-        var client = client_1.getClient();
-        var requestId = client.getNextRequestId();
-        var request = __assign({ requestId: requestId }, options);
-        client.addListener("interactiveList", resolve, requestId);
-        client.post("getInteractiveList", request);
+    return new Promise(function (resolve, reject) {
+        return exports.getMode()
+            .then(function (mode) {
+            if (mode === "authoring") {
+                var client = client_1.getClient();
+                var requestId = client.getNextRequestId();
+                var request = __assign({ requestId: requestId }, options);
+                client.addListener("interactiveList", resolve, requestId);
+                client.post("getInteractiveList", request);
+            }
+            else {
+                reject("getInteractiveList is only available in authoring mode");
+            }
+        });
     });
 };
-/**
- * @todo Implement this function.
- */
 exports.setLinkedInteractives = function (options) {
-    THROW_NOT_IMPLEMENTED_YET("setLinkedInteractives");
+    return new Promise(function (resolve, reject) {
+        return exports.getInitInteractiveMessage()
+            .then(function (initInteractiveMessage) {
+            if (!initInteractiveMessage || initInteractiveMessage.mode !== "authoring") {
+                throw new Error("setLinkedInteractives is only available in authoring mode");
+            }
+            var client = client_1.getClient();
+            var request = __assign({ sourceId: initInteractiveMessage.interactiveItemId }, options);
+            client.post("setLinkedInteractives", request);
+            resolve();
+        }).
+            catch(reject);
+    });
 };
 /**
  * @todo Implement this function.
@@ -32137,4 +32263,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /***/ })
 
 /******/ });
-//# sourceMappingURL=index.js.map
