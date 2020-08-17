df9cdb78f82cf54efdbbb238bb69ff569a58f65f
Unknown <emcelroy@concord.org>
2020-08-17 15:56:23 -0400

    Improve ITSI authoring editor spec test.

--- spec/services/itsi_authoring/editor_spec.rb
+++ spec/services/itsi_authoring/editor_spec.rb
@@ -10,17 +10,40 @@ describe ITSIAuthoring::Editor do
     activity
   }
 
-  let(:page)          { FactoryGirl.create(:interactive_page, name: "page 1", position: 0) }
-  let(:interactive)   { FactoryGirl.create(:managed_interactive) }
+  let(:page)                { FactoryGirl.create(:interactive_page,
+                                                 name: "page 1",
+                                                 position: 0
+                                                 )}
+  let(:library_interactive) { FactoryGirl.create(:library_interactive,
+                                                 name: 'Test Library Interactive',
+                                                 base_url: 'https://concord.org/'
+                                                )}
+  let(:managed_interactive) { FactoryGirl.create(:managed_interactive,
+                                                 library_interactive: library_interactive,
+                                                 url_fragment: "test"
+                                                )}
 
   before(:each) do
-    page.add_interactive interactive
+    page.add_interactive(managed_interactive)
     page.reload
+    activity.pages << page
   end
 
   it "generates JSON for ITSI authoring" do
     editor = ITSIAuthoring::Editor.new(activity)
     itsi_content = editor.to_json
     expect(itsi_content).to have_key(:sections)
+    expect(itsi_content[:sections].length).to eq(1)
+    expect(itsi_content[:sections].first).to match(
+      hash_including(
+        interactives: include(
+          hash_including(
+            type: "managed_interactive",
+            name: "Test Library Interactive",
+            url: "https://concord.org/test"
+          )
+        )
+      )
+    )
   end
 end
