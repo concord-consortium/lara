7734c4246e4dd227d4418012faa00d2b5dc6873b
Unknown <emcelroy@concord.org>
2020-12-04 14:11:44 -0500

    Update spec tests.

--- spec/controllers/publication_controller_spec.rb
+++ spec/controllers/publication_controller_spec.rb
@@ -69,8 +69,6 @@ describe PublicationsController do
         "is_locked"     =>false,
         "url"           =>"http://test.host/activities/#{act_one.id}",
         "source_type"   => "LARA",
-        "tool_id"       =>"",
-        "append_auth_token" => false,
         "create_url"    =>"http://test.host/activities/#{act_one.id}",
         "author_email"  => @user.email,
         "description"   =>"Activity One Description",
@@ -92,8 +90,7 @@ describe PublicationsController do
         "thumbnail_url" =>"thumbnail",
         "is_locked"     =>false,
         "url"           =>"#{ENV["ACTIVITY_PLAYER_URL"]}?activity=http://test.host/api/v1/activities/#{act_two.id}.json",
-        "source_type"   => "Activity Player",
-        "tool_id"       => ENV['ACTIVITY_PLAYER_URL'],
+        "tool_id"       => "https://activity-player.concord.org",
         "append_auth_token" => true
       }
     end
@@ -115,70 +112,85 @@ describe PublicationsController do
     let(:good_response)   { {:status => 201, :body => "", :headers => {} }}
     let(:portal_response) { good_response }
 
-    before(:each) do
-      # @url = controller.portal_url
-      # stub_request(:post, @url)
-      allow(controller).to receive(:find_portal).and_return mock_portal
-      allow(LightweightActivity).to receive(:find).and_return act_one
-      stub_request(:post, publishing_url).
-        with(good_request).
-        to_return(portal_response)
-      stub_request(:post, activity_player_publishing_url).
-        with(ap_good_request).
-        to_return(portal_response)
+    context "when the activity has a LARA runtime" do
+
+      before(:each) do
+        # @url = controller.portal_url
+        # stub_request(:post, @url)
+        allow(controller).to receive(:find_portal).and_return mock_portal
+        allow(LightweightActivity).to receive(:find).and_return act_one
+        stub_request(:post, publishing_url).
+          with(good_request).
+          to_return(portal_response)
+      end
+
+      it "should have proper routing" do
+        expect({:get => "/publications/add/LightweightActivity/1"}).
+          to route_to({
+            :controller       => 'publications',
+            :action           => 'add_portal',
+            :publishable_type => 'LightweightActivity',
+            :publishable_id   => "1"
+        })
+      end
+
+      it "should call 'portal_publish' on the activity" do
+        expect(act_one).to receive(:portal_publish).with(@user, mock_portal, "#{request.protocol}#{request.host_with_port}")
+        get :add_portal, { :publishable_id => act_one.id, :publishable_type => "LightweightActivity" }
+        # should be moved to publishable_spec
+        # expect(act_one.publication_status).to eq('public')
+      end
+
+      # this should be moved to publishable_spec
+      it "should attempt to publish to the correct portal endpoint" do
+        skip "New portal configurations, more testing here"
+        expect(@url).to eq("#{ENV['CONCORD_PORTAL_URL']}/external_activities/publish/v2")
+      end
+
+      # this should be moved to publishable_spec
+      it 'creates a new PortalPublication record' do
+        old_publication_count = act_one.portal_publications.length
+        get :add_portal, { :publishable_id => act_one.id, :publishable_type => "LightweightActivity" }
+        expect(act_one.reload.portal_publications.length).to eq(old_publication_count + 1)
+      end
+
+      # this should be moved to publishable_spec
+      it "should set publication_status to public" do
+        get :add_portal, { :publishable_id => act_one.id, :publishable_type => "LightweightActivity" }
+        # expect(act_one.publication_status).to eq('public')
+      end
     end
 
+    context "when an activity has an Activity Player runtime" do
 
+      before(:each) do
+        # @url = controller.portal_url
+        # stub_request(:post, @url)
+        allow(controller).to receive(:find_portal).and_return mock_portal
+        allow(LightweightActivity).to receive(:find).and_return act_two
+        stub_request(:post, activity_player_publishing_url).
+          with(ap_good_request).
+          to_return(portal_response)
+      end
 
-    it "should have proper routing" do
-      expect({:get => "/publications/add/LightweightActivity/1"}).
-        to route_to({
-          :controller       => 'publications',
-          :action           => 'add_portal',
-          :publishable_type => 'LightweightActivity',
-          :publishable_id   => "1"
-      })
-    end
+      it "should call 'portal_publish' on the activity" do
+        expect(act_two).to receive(:portal_publish).with(@user, mock_portal, "#{request.protocol}#{request.host_with_port}")
+        get :add_portal, { :publishable_id => act_two.id, :publishable_type => "LightweightActivity" }
+        # should be moved to publishable_spec
+        # expect(act_two.publication_status).to eq('public')
+      end
 
-    it "should call 'portal_publish' on the activity" do
-      expect(act_one).to receive(:portal_publish).with(@user, mock_portal, "#{request.protocol}#{request.host_with_port}")
-      get :add_portal, { :publishable_id => act_one.id, :publishable_type => "LightweightActivity" }
-      # should be moved to publishable_spec
-      # expect(act_one.publication_status).to eq('public')
-    end
+      it 'creates a new PortalPublication record for the activity' do
+        old_publication_count = act_two.portal_publications.length
+        get :add_portal, { :publishable_id => act_two.id, :publishable_type => "LightweightActivity" }
+        expect(act_two.reload.portal_publications.length).to eq(old_publication_count + 1)
+      end
 
-    # this should be moved to publishable_spec
-    it "should set publication_status to public" do
-      get :add_portal, { :publishable_id => act_one.id, :publishable_type => "LightweightActivity" }
-      get :add_portal, { :publishable_id => act_two.id, :publishable_type => "LightweightActivity" }
-      # expect(act_one.publication_status).to eq('public')
-    end
-
-    # this should be moved to publishable_spec
-    it "should attempt to publish to the correct portal endpoint" do
-      skip "New portal configurations, more testing here"
-      expect(@url).to eq("#{ENV['CONCORD_PORTAL_URL']}/external_activities/publish/v2")
-    end
-
-    # this should be moved to publishable_spec
-    it 'creates a new PortalPublication record' do
-      old_publication_count = act_one.portal_publications.length
-      get :add_portal, { :publishable_id => act_one.id, :publishable_type => "LightweightActivity" }
-      expect(act_one.reload.portal_publications.length).to eq(old_publication_count + 1)
-    end
-
-    it "should call 'portal_publish' on the activity player runtime activity" do
-      allow(LightweightActivity).to receive(:find).and_return act_two
-      expect(act_two).to receive(:portal_publish).with(@user, mock_portal, "#{request.protocol}#{request.host_with_port}")
-      get :add_portal, { :publishable_id => act_two.id, :publishable_type => "LightweightActivity" }
-      # should be moved to publishable_spec
-      # expect(act_two.publication_status).to eq('public')
-    end
-
-    it 'creates a new PortalPublication record' do
-      old_publication_count = act_two.portal_publications.length
-      get :add_portal, { :publishable_id => act_two.id, :publishable_type => "LightweightActivity" }
-      expect(act_two.reload.portal_publications.length).to eq(old_publication_count + 1)
+      # this should be moved to publishable_spec
+      it "should set publication_status to public for the activity" do
+        get :add_portal, { :publishable_id => act_two.id, :publishable_type => "LightweightActivity" }
+        # expect(act_one.publication_status).to eq('public')
+      end
     end
   end
 end
--- spec/models/lightweight_activity_spec.rb
+++ spec/models/lightweight_activity_spec.rb
@@ -401,8 +401,6 @@ describe LightweightActivity do
         "student_report_enabled" => activity.student_report_enabled,
         "show_submit_button"     => true,
         "is_locked"              => false,
-        "append_auth_token"      => false,
-        "tool_id"                => "",
         "sections"               =>[{"name"=>"#{activity.name} Section", "pages"=>[]}]
       }
     end
@@ -412,22 +410,17 @@ describe LightweightActivity do
       print_url = "#{url}/print_blank"
       ap_url = "#{ENV["ACTIVITY_PLAYER_URL"]}?activity=http://test.host/api/v1/activities/#{activity_player_activity.id}.json"
       {
-        "source_type"            => "Activity Player",
         "type"                   =>"Activity",
         "name"                   => activity_player_activity.name,
-        "description"            => activity_player_activity.description,
         "url"                    => ap_url,
-        "create_url"             => ap_url,
         "author_url"             => author_url,
         "print_url"              => print_url,
         "thumbnail_url"          => thumbnail_url,
-        "author_email"           => activity_player_activity.user.email,
         "student_report_enabled" => activity_player_activity.student_report_enabled,
         "show_submit_button"     => true,
         "is_locked"              => false,
         "append_auth_token"      => true,
-        "tool_id"                => ENV["ACTIVITY_PLAYER_URL"],
-        "sections"               =>[{"name"=>"#{activity_player_activity.name} Section", "pages"=>[]}]
+        "tool_id"                => "https://activity-player.concord.org"
       }
     end
 
@@ -436,7 +429,7 @@ describe LightweightActivity do
     end
 
     it 'returns a simple hash for an Activity Player activity that can be consumed by the Portal' do
-      expect(activity_player_activity.serialize_for_portal('http://test.host')).to eq(ap_simple_portal_hash)
+      expect(activity_player_activity.serialize_for_portal_basic('http://test.host')).to eq(ap_simple_portal_hash)
     end
 
     describe 'pages section' do
