4021b582630a8539d2eec0f534fde9c6be4e7ddb
Noah Paessel <knowuh@gmail.com>
2021-08-26 13:39:35 -0400

    rm debugger

--- lara-typescript/src/stories/authoring-page.stories.tsx
+++ lara-typescript/src/stories/authoring-page.stories.tsx
@@ -33,7 +33,6 @@ export const AuthoringPageStory = (_: any, { context: [state, dispatch] }: ICont
 
   const changeSection = (changes: {section: Partial<ISectionProps>}) => {
     const { section } = changes;
-    debugger;
     const newSections = state.sections.map (s => {
       if (s.id === section.id) {
         return {...s, ...changes.section};
