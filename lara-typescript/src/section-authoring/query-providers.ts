4208aeb1f7bd953931619efe66d16288e023a70f
Noah Paessel <knowuh@gmail.com>
2021-09-15 18:37:36 -0400

    WIP:  Trying Query Providers ...

--- lara-typescript/src/section-authoring/query-providers.ts
+++ lara-typescript/src/section-authoring/query-providers.ts
@@ -1,82 +1,95 @@
 
-import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from "react-query";
+import {useMutation, useQuery, useQueryClient} from "react-query";
 
-interface APIPageResponse extends IPageProps {
-  success: boolean;
+
+import { useState } from "react";
+
+type SectionId = string;
+type PageId = string;
+type ItemId = string;
+
+type RelativePosition = "before" | "after";
+
+interface IHasStringId {
+  id: string;
 }
 
-const APIBase = "/api/v1";
-
-interface IQueryBoundPage extends IPageProps {
-  host?: string;
-}
-
-export const QueryBoundPage = (props: IQueryBoundPage) => {
-  const queryClient = useQueryClient();
-  const host = props.host || "";
-  const prefix = `${host}/${APIBase}`;
-  const {id} = props;
-  const pageSectionsUrl = `${prefix}/get_page_sections/${id}.json`;
-  const updatePageSectionsURL = `${prefix}/set_page_sections/${id}.json`;
-  const createPageSectionUrl = `${prefix}/create_page_section/${id}.json`;
-  const updateSectionUrl = `${prefix}/update_page_section/${id}.json`;
-  const updatePageQueryData = (response: any, variables: any) => {
-    queryClient.setQueryData("authoringPage", response);
-  };
-
-  const updateSections = (nextPage: IPageProps) => {
-    return fetch(updatePageSectionsURL, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(nextPage)
-    }).then(res => {
-      return res.json();
-    });
-  };
-
-  const createSection = () => {
-    return fetch(createPageSectionUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({id})
-    }).then(res => {
-      return res.json();
-    });
-  };
-
-  const _changeSection = (changes: {section: Partial<ISectionProps>}) => {
-    const updateSectionData = {id, section: {...changes.section}};
-    return fetch(updateSectionUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(updateSectionData)
-    }).then(res => {
-      return res.json();
-    });
-  };
-
-  const updatePageSectionsMutation = useMutation(updateSections, {
-    onSuccess: updatePageQueryData
-  });
-
-  const createSectionMutation = useMutation(createSection, {
-    onSuccess: updatePageQueryData
-  });
-
-  const changeSectionMutation = useMutation(_changeSection);
-
-  const { isLoading, error, data} = useQuery("authoringPage", () =>
-    fetch(pageSectionsUrl)
-    .then(res => {
-      return res.json();
-    })
-  );
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Something went wrong: ${error}</div>;
-  const {success, sections } = data as APIPageResponse;
-  const setSections = updatePageSectionsMutation.mutate;
-  const addSection = createSectionMutation.mutate as () => void;
-  const changeSection = changeSectionMutation.mutate;
-  return <AuthoringPage {...{id, sections, setSections, addSection, changeSection}} />;
+const insertAt = <T extends IHasStringId>(thing: T, targetId: string, position: RelativePosition, collection: T[]) => {
+  let index = collection.findIndex(i => i.id === targetId);
+  if (position === "after") index++;
+  return [...collection.slice(0, index), thing, ...collection.slice(index)];
 };
+
+const removeFrom = <T extends IHasStringId>(targetId: string, collection: T[]) => {
+  return collection.filter(i => i.id !== targetId);
+};
+
+export interface ISectionItem {
+  id: ItemId;
+  title?: string;
+}
+
+export interface ISection {
+  id: SectionId;
+  items: ISectionItem[];
+  title?: string;
+}
+
+export interface IPage {
+  id: PageId;
+  sections: ISection[];
+  title?: string;
+}
+
+export type IPageList = IPage[];
+
+export interface IPageAuthoringAPI {
+  // Simplified page API:
+  getPages: () => QueryObserverIdleResult<IPageList, Error>;
+  getPage: (id: PageId) => IPage|null;
+  createPage: () => IPage;
+  deletePage: (id: PageId) => IPageList;
+  // copyPage: (id: PageId) => IPageList;
+  // updatePage: (id: PageId, changes: Partial<IPage>) => IPageList;
+  // movePage: (id: PageId, dest: IPageDestination) => IPageList;
+  // pages: IPageList;
+}
+
+export const UseOfflinePageAPIQueries = (): IPageAuthoringAPI => {
+  let pages: IPageList = [];
+  let counter: 0;
+  const pagesCacheKey = "pages";
+  const queryClient = useQueryClient();
+  const nextId = () => `${counter++}`;
+  const createPage = () => {
+    return {
+      id: nextId(),
+      title: `Page ${counter}`,
+      sections: []
+    };
+  };
+
+  const getPage = (id: PageId) => {
+    return pages.find(p => p.id === id) || null;
+  };
+
+  const deletePage = (id: PageId) => {
+    pages = pages.filter(p => p.id !== id);
+    queryClient.invalidateQueries(pagesCacheKey);
+    return Promise.resolve(pages);
+  };
+
+  const getPages = () => {
+    return Promise.resolve(pages);
+  };
+
+  // Return the interface
+  return {
+    getPages: useQuery<IPageList, Error>("pages", getPages),
+    getPage: (id: PageId) => useQuery([pagesCacheKey,id], getPage(id)),
+    createPage,
+    deletePage
+  };
+}
+
+export const OfflineQueryProvider = {}
