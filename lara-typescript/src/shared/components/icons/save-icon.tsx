badd48dd3e0bd47cc2a8f8b1acd55fe6ec4b4887
Unknown <emcelroy@concord.org>
2021-09-24 16:17:23 -0400

    Page Settings Menu
    PT Story: https://www.pivotaltracker.com/story/show/179597704
    
    [#179597704]
    
    Adds a page settings menu. Also adds a header for the page editing view that includes the page title and settings menu toggle button.

---
+++ lara-typescript/src/shared/components/icons/save-icon.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+const kDefaultHight = "1em";
+const kDefaultWidth = "1em";
+
+export interface ISaveProps {
+  height?: string;
+  width?: string;
+}
+
+export const Save = (props: ISaveProps) => {
+  const height = props.height ? props.height : kDefaultHight;
+  const width = props.width ? props.width : kDefaultWidth;
+
+  return(
+    <svg
+      aria-hidden={true}
+      className="icon"
+      fill="white"
+      focusable="false"
+      height={height}
+      width={width}
+      role="img"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg">
+      <g>
+        <path d="M15.173 2H4c-1.101 0-2 .9-2 2v12c0 1.1.899 2 2 2h12c1.101 0 2-.9 2-2V5.127L15.173 2zM14 8c0 .549-.45 1-1 1H7c-.55 0-1-.451-1-1V3h8v5zm-1-4h-2v4h2V4z"/>
+      </g>
+    </svg>
+  );
+};
