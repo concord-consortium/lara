0723b023a867e3d688acd7143d1b23a0a5efeb9e
Noah Paessel <knowuh@gmail.com>
2021-10-04 17:54:55 -0400

    179774153 undo unwanted change to section-item.tsx
    #179774153
    https://www.pivotaltracker.com/story/show/179774153

--- lara-typescript/src/section-authoring/components/section-item.tsx
+++ lara-typescript/src/section-authoring/components/section-item.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { GripLines } from "../../shared/components/icons/grip-lines";
-import { AuthoringSection } from "./authoring-section";
 
 import "./section-item.scss";
 
@@ -86,11 +85,6 @@ export const SectionItem: React.FC<ISectionItemProps> = ({
     deleteFunction?.(id);
   };
 
-  const renderEmbeddable = () => {
-    if(type == "text-block" ) {
-      return <TextBlock {...data } />
-    }
-  }
   return(
     <div className="sectionItemContainer">
       <header className="sectionItemMenu">
@@ -98,7 +92,6 @@ export const SectionItem: React.FC<ISectionItemProps> = ({
           <GripLines />
           <h4>{id} - {title} - {renderTitle()}</h4>
         </div>
-        { renderEmbeddable() }
         <div className="menuEnd">
           <ul>
             <li><button onClick={toggleCollapse}>Collapse</button></li>
