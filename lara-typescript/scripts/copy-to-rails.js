96303ea634d1afe2e3318e9f8b1d9ee18793f656
Doug Martin <dmartin@concord.org>
2021-10-05 11:06:31 -0400

    Fixed update of public/example-interactives folder on build

--- lara-typescript/scripts/copy-to-rails.js
+++ lara-typescript/scripts/copy-to-rails.js
@@ -1,6 +1,9 @@
 var cpx = require("cpx");
+var rimraf = require("rimraf");
 
 cpx.copy("dist/lara-typescript.js","../app/assets/javascripts/",{update:true});
 cpx.copy("dist/lara-typescript.css","../app/assets/stylesheets/",{update:true});
-cpx.copy("dist/example-interactives/index.*","../public/example-interactives/",
-         {clean: true, update:true});
+cpx.copy("dist/example-interactives/**/*.*","../public/example-interactives/", {clean: true, update:true});
+
+/* remove typescript .d.ts files */
+rimraf.sync("dist/example-interactives/src/**/*.*");
