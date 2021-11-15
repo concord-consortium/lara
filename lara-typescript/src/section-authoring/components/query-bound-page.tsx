7edb08ca12b034147f1c18763ba7c4d0fc3a845e
Noah Paessel <knowuh@gmail.com>
2021-11-15 15:54:59 -0500

    WIP:  Set page when launched
    TODO: Undo this commit, and see if we can just do this by looking at the URL instead ...

--- lara-typescript/src/section-authoring/components/query-bound-page.tsx
+++ lara-typescript/src/section-authoring/components/query-bound-page.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { APIContainer } from "../containers/api-container";
 import { AuthoringPageUsingAPI, IPageProps } from "./authoring-page";
 
-interface IQueryBoundPage extends IPageProps {
+export interface IQueryBoundPage extends IPageProps {
   host?: string;
   activityId?: string;
 }
@@ -10,8 +10,9 @@ interface IQueryBoundPage extends IPageProps {
 export const QueryBoundPage = (props: IQueryBoundPage) => {
   const host = props.host || "https://app.lara.docker";
   const activityId = props.activityId || "55";
+  const pageId = props.id;
   return (
-    <APIContainer activityId={activityId} host={host}>
+    <APIContainer activityId={activityId} host={host} pageId={pageId}>
       <AuthoringPageUsingAPI />
     </APIContainer>
   );
--- lara-typescript/src/section-authoring/index.tsx
+++ lara-typescript/src/section-authoring/index.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthoringSection, ISectionProps } from "./components/authoring-section";
-import { IPageProps } from "./components/authoring-page";
-import { QueryBoundPage } from "./components/query-bound-page";
+import { QueryBoundPage, IQueryBoundPage } from "./components/query-bound-page";
 const renderAuthoringSection = (root: HTMLElement, props: ISectionProps) => {
   return ReactDOM.render(<AuthoringSection {...props} />, root);
 };
 
-const renderAuthoringPage = (root: HTMLElement, props: IPageProps) => {
+const renderAuthoringPage = (root: HTMLElement, props: IQueryBoundPage) => {
   const App = <QueryBoundPage {...props} />;
   return ReactDOM.render(App, root);
 };
