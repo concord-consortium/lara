17d6924fd253958958f184890c86867d0f8c5a7c
Unknown <emcelroy@concord.org>
2022-02-07 16:09:13 -0500

    Update Project form to support AP footer styling
    PT Story: https://www.pivotaltracker.com/story/show/181059664
    
    Adds several fields to Projects for use in Activity Player footer. Removes unused Help field from Projects.

---
+++ db/migrate/20220207022454_add_ap_footer_fields_to_projects.rb
@@ -0,0 +1,21 @@
+class AddApFooterFieldsToProjects < ActiveRecord::Migration
+  def up
+    add_column :projects, :copyright, :text
+    add_column :projects, :copyright_image_url, :string
+    add_column :projects, :collaborators, :text
+    add_column :projects, :funders_image_url, :string
+    add_column :projects, :collaborators_image_url, :string
+    add_column :projects, :contact_email, :string
+    remove_column :projects, :help
+  end
+
+  def down
+    remove_column :projects, :copyright, :text
+    remove_column :projects, :copyright_image_url, :string
+    remove_column :projects, :collaborators, :text
+    remove_column :projects, :funders_image_url, :string
+    remove_column :projects, :collaborators_image_url, :string
+    remove_column :projects, :contact_email, :string
+    add_column :projects, :help, :text
+  end
+end
