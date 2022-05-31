aba578d7a6683cbe353c9603a927b69d30621fb1
Noah Paessel <knowuh@gmail.com>
2022-05-31 13:17:00 -0400

    179644118 - rm unused changes (of iframing erb)

--- app/controllers/embeddable/embeddable_plugins_controller.rb
+++ app/controllers/embeddable/embeddable_plugins_controller.rb
@@ -1,29 +1,10 @@
 class Embeddable::EmbeddablePluginsController < Embeddable::EmbeddablesController
   before_filter :set_embeddable
 
-
-  # modified from application_controller.rb `respond_with_edit_form`
-
-  def render_plugin_form(css_class = nil, container_class = nil)
-    respond_to do |format|
-      format.js do
-        render(
-          json: {
-            html: render_to_string('edit'),
-            css_class: css_class,
-            container_class: container_class
-          },
-          content_type: 'text/json'
-        )
-      end
-      format.html { render 'edit', layout: 'minimal' }
-    end
-  end
-
   def edit
     # set theme for plugin preview
     current_theme
-    render_plugin_form("allow-full-width", "opaque-background")
+    respond_with_edit_form("allow-full-width", "opaque-background")
   end
 
   private
