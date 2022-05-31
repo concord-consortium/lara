20a2244d8c18d05037a0746220961e8dab4c6492
Noah Paessel <knowuh@gmail.com>
2022-05-31 18:06:40 -0400

    179644118 - Add windowshade preview

--- lara-typescript/src/section-authoring/components/plugin-preview.tsx
+++ lara-typescript/src/section-authoring/components/plugin-preview.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { ISectionItem, ITextBlockData } from "../api/api-types";
-
+import { ISectionItem } from "../api/api-types";
+import { initPlugin, setNextPluginLabel } from "../../plugins/plugins";
+import { IPluginRuntimeContextOptions } from "../../plugins/plugin-context";
 export interface IPluginPreviewProps {
   pageItem: ISectionItem;
 }
@@ -11,24 +12,90 @@ export const PluginPreview: React.FC<IPluginPreviewProps> = ({
 
   const wrapperClasses = "plugin-preview-wrapper";
 
-  const data = pageItem.data;
-  const pageItemDeets = JSON.stringify(pageItem, null, 2);
-  const dataString = JSON.stringify(data, null, 2);
+  const { data } = pageItem;
+  const { componentLabel, url, label, name, authorData, pluginId} = data;
 
-  const name = "Plugin - Teacher Edition: WindowShade";
+  const wrappedDiv = React.useRef<HTMLDivElement>(null);
+  const containerDiv = React.useRef<HTMLDivElement>(null);
+  const firebaseJwtUrl = "TODO:firebaseJwtUrl"; // NP 2022-05-26 TODO: from whence?
+  const portalJwtUrl = "TODO:portalJwtUrl";     // NP 2022-05-26 TODO: from whence?
+
+  const effectDeps = [
+    wrappedDiv.current, containerDiv.current, firebaseJwtUrl,
+    portalJwtUrl, label, url, authorData
+  ];
+
+  // Hack: The only way to express teacher edition mode AFIK is to add it to
+  // the query string. TE won't display without that query param.
+  const maybeAddTeacherEditionParams = () => {
+    if (window.location.search.indexOf("mode=teacher-edition") < 0) {
+      window.location.search = "?mode=teacher-edition";
+    }
+  };
+
+  React.useEffect(() => {
+    if (!(url && containerDiv.current)) return;
+    const script = document.createElement("script");
+    maybeAddTeacherEditionParams();
+    script.onload = () => {
+      if (authorData) {
+        setNextPluginLabel(label);
+        const pluginContext: IPluginRuntimeContextOptions = {
+          type: "runtime",
+          learnerState: null,
+          runId: 0,
+          name,
+          url,
+          pluginId,
+          componentLabel,
+          authoredState: authorData || null,
+          // Most of the runtime properties are null or blank
+          // Because we are just previewing.
+          learnerStateSaveUrl: "",
+          remoteEndpoint: "",
+          userEmail: "",
+          classInfoUrl: "",
+          embeddablePluginId: null,
+          resourceUrl: "",
+          offlineMode: false,
+          container: containerDiv.current!,
+          wrappedEmbeddable: null,
+          /**** TODO: Handle Wrapped Embeddable for other plugin types
+           #{!wrapped_embeddable ? 'null' : "{
+            container: wrappedDiv,
+            laraJson: #{wrapped_embeddable_lara_json},
+            interactiveStateUrl: null,
+            interactiveAvailable: #{!click_to_play}
+          }"},
+            if ($('#wrapped_embeddable').length > 0) {
+            const interactiveContainer = $('#wrapped_embeddable').find('.interactive-container')[0];
+            renderInteractiveAuthoringPreview(interactiveContainer, {
+            interactive: #{wrapped_embeddable
+                && defined?(wrapped_embeddable.to_authoring_preview_hash)
+                ? wrapped_embeddable.to_authoring_preview_hash().to_json
+                : 'null'},
+            user: #{userInfo.to_json}
+          });
+        }
+          **/
+          firebaseJwtUrl,
+          portalJwtUrl
+        };
+        initPlugin(label, pluginContext);
+      }
+    },
+    script.onerror = (e) => alert(`Unable to load plugin script: ${url} ${e} ${script.src}`);
+    script.src = url;
+    document.head.append(script);
+  }, effectDeps);
+
   return (
     <div className={wrapperClasses}>
-      <div className="textBlockName">
-        {name}
+      {name} <br/>
+      <div className="plugin-authoring-container">
+        <div ref={containerDiv}/>
+        <div ref={wrappedDiv} />
       </div>
-      {/* <div className="data">
-        <pre style={{overflow: "auto"}}>
-          {dataString}
-        </pre>
-        <pre style={{overflow: "auto"}}>
-          {pageItemDeets}
-        </pre>
-      </div> */}
     </div>
   );
 };
