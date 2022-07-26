2ce7a4ae79cc1d878add790e7b7e4852f452c42f
Unknown <emcelroy@concord.org>
2022-07-26 12:20:06 -0400

    Add associations to embeddable plugins and external scripts.

--- app/models/embeddable/external_script.rb
+++ app/models/embeddable/external_script.rb
@@ -7,6 +7,8 @@ module Embeddable
     belongs_to :approved_script
     has_many :page_items, :as => :embeddable, :dependent => :destroy
     has_many :interactive_pages, :through => :page_items
+    has_one :converted_interactive, class_name: "ManagedInteractive", as: :legacy_ref
+    has_many :embeddable_plugins, class_name: "Embeddable::EmbeddablePlugin", as: :embeddable
     delegate :name,  to: :approved_script, allow_nil: true
     delegate :label, to: :approved_script, allow_nil: true
     delegate :url,   to: :approved_script, allow_nil: true
