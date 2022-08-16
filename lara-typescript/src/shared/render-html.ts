6718c741b563d74d614fc8eab56dbd84b5152dc3
Unknown <emcelroy@concord.org>
2022-08-16 15:27:23 -0400

    Fix iframes not rendered in the authoring preview.
    PT Story: https://www.pivotaltracker.com/story/show/182908649
    
    [#182908649]
    
    Adds iframe to the default set of allowed HTML tags, and adds some related attributes to the set of allowed attributes.

--- lara-typescript/src/shared/render-html.ts
+++ lara-typescript/src/shared/render-html.ts
@@ -2,5 +2,5 @@ import * as DOMPurify from "dompurify";
 import parse from "html-react-parser";
 
 export function renderHTML(html: string) {
-  return parse(DOMPurify.sanitize(html || ""));
+  return parse(DOMPurify.sanitize(html || "", {ADD_TAGS: ["iframe"], ADD_ATTR: ["allowfullscreen", "frameborder", "scrolling", "target"]}));
 }
