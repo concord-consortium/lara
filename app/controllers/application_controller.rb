eeeb4f0ff30850396d3da867f5a40c546d0f9b8a
pjanik <janikpiotrek@gmail.com>
2024-05-06 14:58:09 +0200

    chore: cleanup potentially misleading references to Shutterbug server [PT-187531185]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,6 @@ class ApplicationController < ActionController::Base
   before_filter :reject_old_browsers, :except => [:bad_browser]
   before_filter :set_locale
   before_filter :store_auto_publish_url # to enable auto publishing to build an url from the request object
-  before_filter :set_api_urls
   after_filter :log_session_after
 
   helper_method :resource_name
@@ -325,10 +324,6 @@ class ApplicationController < ActionController::Base
 
   def store_auto_publish_url
     Thread.current[:auto_publish_url] = "#{request.protocol}#{request.host_with_port}"
-  end
-
-  def set_api_urls
-    gon.shutterbugURI = ENV['SHUTTERBUG_URI']
   end
 
   def unauthorized_response_data(action, resource)
