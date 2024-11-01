1b54ac4f4e8657511f7825b7b6b21037a8084bb5
Ethan McElroy <emcelroy@concord.org>
2024-11-01 14:56:16 +0000

    chore: reinstate delayed job logging

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -5,6 +5,6 @@ Delayed::Worker.sleep_delay = 1
 # Delayed::Worker.max_attempts = 25
 # Delayed::Worker.max_run_time = 4.hours
 
-# Delayed::Worker.plugins << Delayed::Plugins::TaggedLogging
+Delayed::Worker.plugins << Delayed::Plugins::TaggedLogging
 
 Delayed::Worker.delay_jobs = Rails.env.production? || !!ENV['DELAYEDJOB']
