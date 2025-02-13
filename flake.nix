35ffd346c78c0bc01dc6739d382a2f2556b55af2
Ethan McElroy <emcelroy@concord.org>
2025-02-13 13:07:10 +0000

    chore: update flake.nix file for Ruby 2.7 upgrade

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-ancient-ruby = {
-      url = "github:nixos/nixpkgs/0bcbb978795bab0f1a45accc211b8b0e349f1cdb";
-      flake = false;
-    };
+    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
     nixpkgs-ancient-node = {
       url = "github:nixos/nixpkgs/22f65339f3773f5b691f55b8b3a139e5582ae85b";
       flake = false;
@@ -12,14 +9,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils, ... } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pkgs-ancient-node = import inputs.nixpkgs-ancient-node {
           inherit system;
         };
-        pkgs-ancient-ruby = import inputs.nixpkgs-ancient-ruby {
+        pkgs-ruby = import nixpkgs-ruby {
           inherit system;
         };
       in
@@ -29,8 +26,8 @@
           packages = (with pkgs; [
             act
             pkgs-ancient-node.nodejs-10_x # Node.js v10.15.3
-          ]) ++ (with pkgs-ancient-ruby; [
-            ruby_2_7 # Ruby v2.7.0
+          ]) ++ (with pkgs-ruby; [
+            ruby_2_7 # Ruby v2.7 from nixpkgs-ruby
             glibc # Includes 'libcrypt'
             libffi # Needed for 'ffi' Gem
             mysql57
