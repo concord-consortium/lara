6a233a986da4a951647b3d5244e08740bcce8c1d
Ethan McElroy <emcelroy@concord.org>
2025-02-14 02:24:16 +0000

    chore: pin mysql57 in flake.nix

--- flake.nix
+++ flake.nix
@@ -9,19 +9,20 @@
       url = "github:nixos/nixpkgs/22f65339f3773f5b691f55b8b3a139e5582ae85b";
       flake = false;
     };
+    nixpkgs-mysql57 = {
+      url = "github:nixos/nixpkgs/df2176f7f3f748974f24916df29bb70763d89734";
+      flake = false;
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-mysql57, ... } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-ancient-node = import inputs.nixpkgs-ancient-node {
-          inherit system;
-        };
-        pkgs-ancient-ruby = import inputs.nixpkgs-ancient-ruby {
-          inherit system;
-        };
+        pkgs-ancient-node = import inputs.nixpkgs-ancient-node { inherit system; };
+        pkgs-ancient-ruby = import inputs.nixpkgs-ancient-ruby { inherit system; };
+        pkgs-mysql57 = import inputs.nixpkgs-mysql57 { inherit system; };
       in
       {
         formatter = pkgs.nixpkgs-fmt;
@@ -33,7 +34,7 @@
             ruby_3_3 # Ruby v3.3.7
             glibc # Includes 'libcrypt'
             libffi # Needed for 'ffi' Gem
-            mysql57
+            pkgs-mysql57.mysql57
             zlib
             openssl
             sqlite
