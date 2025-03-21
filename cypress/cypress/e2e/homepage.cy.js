92c8aa2f0f1d13256ad19c738f4b807dbb653c30
nstclair-cc <20171905+nstclair-cc@users.noreply.github.com>
2025-03-21 10:46:30 -0700

    added additional checks for the homepage in LARA

--- cypress/cypress/e2e/homepage.cy.js
+++ cypress/cypress/e2e/homepage.cy.js
@@ -5,7 +5,23 @@ describe('homepage spec', () => {
 
   it('should render the homepage', () => {
     cy.contains('Authoring');
-  })
+    // check the header
+    cy.get('[data-cy="authoring-header"]')
+      .should('exist')
+      .and('be.visible')
+      .within(() => {
+        cy.get('.project-logo').should('exist');
+        cy.get('.login-link').should('exist');
+      });
+    
+    // Check the bottom header section
+    cy.get('.bottom-header').should('exist').within(() => {
+      cy.get('[data-testid="jump-to-label"]').should('have.text', 'Jump to:');
+      cy.get('[data-testid="jump-to-sequences"]').should('have.text', 'Sequences');
+      cy.get('[data-testid="jump-to-glossaries"]').should('have.text', 'Glossaries');
+      cy.get('[data-testid="jump-to-rubrics"]').should('have.text', 'Rubrics');
+    });
+  });
 
   it('should allow log in and out with an account', () => {
     cy.getAccountOwnerName().should('not.exist');
